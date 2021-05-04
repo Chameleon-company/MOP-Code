4d584613b2088e2ca5f5f9d6a321b0a6addb242a
rmaamatya <rohanneps@gmail.com>
2021-05-04 22:11:30 +1000

    Added Global configuration file for config-driven execution
    Former-commit-id: ae7df0fb7d3f2c7c1ef81416af54e75e4206229a [formerly cb7c82181a6322e8bf537773238747737b0a288f]
    Former-commit-id: f15eec9503c4f815d710343185f336dc1d602f81

--- T1_2021/sub_team_a/etl_automation/config.py
+++ T1_2021/sub_team_a/etl_automation/config.py
@@ -1,22 +1,48 @@
 
+
+### Extraction ######################################################
+
 # Olympia PARK
 STATION_CODE = '086338'
 
 FEAT_NCCOBS_CODES = {
 	'rainfall': 136,
-	'temperature' : 122,
+	'max_temp' : 122,
+	'min_temp' : 123,
 	'solar_exposure' : 193
 
 }
 
 FEAT_PC_CODES = {
 	'rainfall': -1490874013,
-	'temperature' : -1490871123,
+	'max_temp' : -1490871123,
+	'min_temp' : -1490871319,
 	'solar_exposure' : -1490889015
 
 }
 
 
-
 # BOM DOWNLOAD URL PATTERN
 BOM_DOWNLOAD_BASE_URL = 'http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_display_type=dailyZippedDataFile&p_stn_num={}&p_c={}&p_nccObsCode={}&p_startYear='
+
+
+
+EXTRACTION_DIR = 'extraction'
+#####################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+### EXECUTION #######################################################
+
+OUTPUT_DIR = 'op'
+#####################################################################
