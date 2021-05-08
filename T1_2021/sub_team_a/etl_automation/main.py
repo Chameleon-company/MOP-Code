be6b6cf7e3915ebd8dc7c529d514be27677d7370
rmaamatya <rohanneps@gmail.com>
2021-05-08 15:58:37 +1000

    Added module for handling data imputations
    Former-commit-id: caf5ffa4b7c89ad5467588ae8871de405e989956 [formerly e92603956d3e0a43a319c6dac6775a7f863062c5]
    Former-commit-id: a6852aae998e21c54b876861eba1b9afde557269

--- T1_2021/sub_team_a/etl_automation/main.py
+++ T1_2021/sub_team_a/etl_automation/main.py
@@ -1,8 +1,9 @@
 from extraction import download_datasets
-from transformation import merge_data_sources
+from transformation import merge_data_sources, clean_data
 import config as cfg
 
 
 if __name__ == '__main__':
-	download_datasets.main(cfg.OUTPUT_DIR, cfg)
-	merge_data_sources.main(cfg.OUTPUT_DIR, cfg)
+	# download_datasets.main(cfg)
+	merge_data_sources.main(cfg)
+	clean_data.main(cfg)
