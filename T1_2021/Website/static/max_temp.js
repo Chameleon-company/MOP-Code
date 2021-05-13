144274186f30b102e3b958749dbb0bad4e17955e
aladeinde <aladeinde@deakin.edu.au>
2021-05-14 03:11:38 +1000

    Updating mini and maximum temperature pages
    Former-commit-id: 9e6681fdf9ec6fead0ffb62055ff96c348a3ad40 [formerly e29dff1ee985abea5c201d910b3983263e934b42]
    Former-commit-id: 9b5c9ff1491449a36f92cd44969b4f9ce46b4103

---
+++ T1_2021/Website/static/max_temp.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	$(chart_id).highcharts({
+		chart: chart,
+		title: title,
+		xAxis: xAxis,
+		yAxis: yAxis,
+		series:[{name: 'Maximum Temperature', data: data,  pointStart: Date.UTC(2015, 0, 1) , pointInterval: 24 * 3600 * 1000 }],
+	});
+})
+
+
+$(document).ready(function() {
+	$(chart_id_5).highcharts({
+		chart: chart5,
+		title: title5,
+		xAxis: xAxis5,
+		yAxis: yAxis5,
+		series: series5,
+		tooltip: {headerFormat: '{point.key:%b\'%y}<br/>', shared: true ,         formatter: function () {
+            return 'The frequency of RRP between the range of <b>' + this.x +
+                '  </b> dollars is <b>' + this.y + '</b>';
+
+        }},
+	});
+});
+
