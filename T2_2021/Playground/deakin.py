0ad67f1015e20db2eec8f8f501a8dbed533d5823
Oscarwu <wuyuta@deakin.edu.au>
2021-08-12 23:21:17 +1000

    a) in query fucntion, add url,page-views  attribute b) modified summary table function
    Former-commit-id: 33d3995751c7c0a45c0127e1c4cdf0f78bda449b [formerly 94d9736eee2c3cabeb1a37f0c6b12c4e24e3dcdf]
    Former-commit-id: ec98f6f2995590e8570f85a4319f03f6ce9edf2d

--- T2_2021/Playground/deakin.py
+++ T2_2021/Playground/deakin.py
@@ -4,7 +4,9 @@ import pandas as pd
 from sodapy import Socrata
 
 
-def summary_table():
+def top_table(top = 10):
+
+    ''' return top # downloads of datasets  '''
     client = Socrata("data.melbourne.vic.gov.au", #domain
                 'z99BiHe97JrarLpbpqRISffyr',  #app token
                 username="4l7fysec6unzmqbs1n9ulbdsz", #api id
@@ -17,6 +19,8 @@ def summary_table():
     _page_views = []
     _downloadCount = []
     _features = []
+    _link = []
+    _view = []
     
     
     for a in client.datasets(): #222
@@ -37,23 +41,53 @@ def summary_table():
         
         #print('features name', a['resource']['columns_name'])
     
-        
+        _link.append(a['permalink'])
         #print('download count',a['resource']['download_count'])
         _downloadCount.append(a['resource']['download_count'])
     
         _features.append(a['resource']['columns_name'])
         
+        _view.append(a['resource']['page_views']['page_views_last_month'])
     sum_df = pd.DataFrame({'Name':_name,
                     'id': id_,
-                    'updatead_At':_updatedAt,
-                    'createAt':_createAt,
+                    #'updatead_At':_updatedAt,
+                    #'createAt':_createAt,
+                    "link": _link,
                  #   'pageViews':_page_views,
                #     "attributes":_features,
-                    "downloadCount":_downloadCount
+                    "downloadCount":_downloadCount,
+                    'page_views_last_month':_view
                     })
+
+    sum_df = sum_df.sort_values(['downloadCount'], ascending=False).head(top)
+    sum_df = sum_df.reset_index().drop('index', axis=1)
     return sum_df, client
 
-if __name__ == 'main':
-    summary_table()
+def keyword_search(keywords= None):
+    if keywords is None:
+        print('Please typing valid dataset name')
+    else:
+        sum_table, client= top_table()
+        rows = []
+        for each_dataset in client.datasets():
+        #for key_name in each_dataset['resource']:
+            #    print(key_name,":" ,each_dataset['resource'][str(key_name)])    
+            if each_dataset['resource']['name'].lower().find("street".lower()) != -1:
+            # print('name: ' , each_dataset['resource']['name'],', id:' , each_dataset['resource']['id'])
+                rows.append([each_dataset['resource']['name'], 
+                each_dataset['resource']['id'],
+                each_dataset['resource']['download_count'],
+                each_dataset['permalink']
+                ]
+                )
+        df = pd.DataFrame(rows, columns=["Name", "Id", 'Downloads','link'])
 
+        return df
+
+
+
+if __name__ == 'main':
+
+    keyword_search('parking')
+    print(df)
 
