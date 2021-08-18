4bf5601f8f6e32eb0e53da2a35a62f4020dcafc0
Josh Sansom <jsansomsherwill@deakin.edu.au>
2021-08-18 14:07:45 +1000

    changed the name of test_search to dataset_search for better readability
    Former-commit-id: e0ec6f8f1f107f6df7a92e2ee71a53215819a0fc [formerly 10e6303b899db49314bc3f0835182af67f18f9ab]
    Former-commit-id: 56ab4b860070502f3de0e35caaeadbdf0b2cfba8

--- T2_2021/webapp/flaskr/home.py
+++ T2_2021/webapp/flaskr/home.py
@@ -2,7 +2,7 @@ import functools
 from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify)
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskr.test_search import keyword_search
+from flaskr.dataset_search import keyword_search
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
