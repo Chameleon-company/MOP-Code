78b1beb4aef85c78a2c55a85bc3a1e14c994d452
Oscarwu <wuyuta@deakin.edu.au>
2021-08-25 01:56:44 +1000

    a) meta data api b) inspect function c)search function d)download function f)parser argument g)toruial in lookme.md and accept parameter in terminal
    Former-commit-id: c1af8a235af469fe447239076f8e60ccfa1e1848 [formerly 4666d9c8363b4e2c029dc6177cdd819ecc6cf8c7]
    Former-commit-id: f34eb6c4432d2f270c685f5c06f6ac746ce90d0c

---
+++ T2_2021/Playground/Oscar/deakin.py
@@ -0,0 +1,113 @@
+import pandas as pd
+from sodapy import Socrata
+
+
+def top_table(top = 20):
+
+    ''' return top # downloads of datasets  '''
+    client = Socrata("data.melbourne.vic.gov.au", #domain
+                'z99BiHe97JrarLpbpqRISffyr',  #app token
+                username="4l7fysec6unzmqbs1n9ulbdsz", #api id
+                password="21kqwm0898v3yjkskdd2i940fc0g7quvr6rg80zalcyuhc1v4n") #api secrete
+
+    _name = []
+    id_ = []
+    _updatedAt = []
+    _createAt = []
+    _page_views = []
+    _downloadCount = []
+    _features = []
+    _link = []
+    _view = []
+    
+    
+    for a in client.datasets(): #222
+        
+        #print('dataset_name',a['resource']['name'])
+        _name.append(a['resource']['name'])
+        #print('unique identifier',a['resource']['id'])
+        id_.append(a['resource']['id'])
+    
+        #print('updatedAt',a['resource']['updatedAt'])
+        _updatedAt.append(a['resource']['updatedAt'])
+    
+        #print('createAt', a['resource']['createdAt'])
+        _createAt.append(a['resource']['createdAt'])
+        
+        #print('page_views', a['resource']['page_views'])
+        _page_views.append(a['resource']['page_views'])
+        
+        #print('features name', a['resource']['columns_name'])
+    
+        _link.append(a['permalink'])
+        #print('download count',a['resource']['download_count'])
+        _downloadCount.append(a['resource']['download_count'])
+    
+        _features.append(a['resource']['columns_name'])
+        
+        _view.append(a['resource']['page_views']['page_views_last_month'])
+    sum_df = pd.DataFrame({'Name':_name,
+                    'id': id_,
+                    #'updatead_At':_updatedAt,
+                    #'createAt':_createAt,
+                    "link": _link,
+            #   'pageViews':_page_views,
+            #     "attributes":_features,
+                    "downloadCount":_downloadCount,
+                    'page_views_last_month':_view
+                    })
+
+    sum_df = sum_df.sort_values(['page_views_last_month'], ascending=False).head(top)
+    sum_df = sum_df.reset_index().drop('index', axis=1)
+    return sum_df, client
+
+def keyword_search(keywords= None):
+    if keywords is None:
+        print('Please typing valid dataset name')
+    else:
+        sum_table, client= top_table()
+        rows = []
+        for each_dataset in client.datasets():
+            check_list = []
+            for i in keywords.lower().split():
+                if each_dataset['resource']['name'].lower().find(i) != -1:
+                    check_list.append(1)
+                else:
+                    check_list.append(0)
+            if any(check_list):
+                rows.append([each_dataset['resource']['name'], 
+                each_dataset['resource']['id'],
+                each_dataset['resource']['download_count']
+            ])
+        df = pd.DataFrame(rows, columns=["Name", "Id", 'Downloads'])
+        df = df.sort_values(['Downloads'], ascending=False)
+        df = df.reset_index().drop('index', axis=1)
+        return df
+
+def data_inspect(keywords= None, client=None):
+    if keywords is None:
+        print('Please typing valid dataset name')
+    else:
+        rows = []
+        for each_dataset in client.datasets():
+            check_list = []
+            for i in keywords.lower().split():
+                if each_dataset['resource']['name'].lower().find(i) != -1:
+                    check_list.append(1)
+                else:
+                    check_list.append(0)
+            if any(check_list):
+                rows.append([each_dataset['resource']['name'], 
+                each_dataset['resource']['id'],
+                each_dataset['resource']['download_count']
+            ])
+        df = pd.DataFrame(rows, columns=["Name", "Id", 'Downloads'])
+        df = df.sort_values(['Downloads'], ascending=False)
+        df = df.reset_index().drop('index', axis=1)
+        return df
+
+
+
+#if __name__ == 'main':
+
+
