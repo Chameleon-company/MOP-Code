f36a692a2ccf4af8683659974b51b90ab6503a3c
sansomguy <jsansomsherwill@deakin.edu.au>
2021-09-22 20:24:36 +1000

    Added daily file fetching for parking sensor data
    Former-commit-id: d1c433dd73a32b59d285ba7ce475fb661fbf8d83 [formerly 733e8484f213d339456ab6a06443053e8743e4c8]
    Former-commit-id: 10d4e1f273fa49188f83a06ffa9278624ab6801d

--- T2_2021/webapp/flaskr/parking_sensor/parking_sensor_data.py
+++ T2_2021/webapp/flaskr/parking_sensor/parking_sensor_data.py
@@ -1,15 +1,35 @@
 import boto3
 import pandas as pd
+import concurrent.futures
+from datetime import datetime, timedelta
+import threading
+
+thread_local = threading.local()
+
+def get(bucket, resource):
+    
+    def get_key(key):
+        thread_local.reader = resource.Object(bucket, key).get()
+        return thread_local.reader
+
+    return get_key
 
 ''' Returns Parking sensor csv'''
 def get_parking_sensor_data():
-    df = pd.read_csv('flaskr/parking_sensor/data/parkingsensor.csv', parse_dates=True, infer_datetime_format=True)
-    return df
-    # bucket = 'opendataplayground.deakin'
-    # # get existing dataframe from csv on S3
-    # s3_resource = boto3.resource('s3')
-    # key = 'parkingsensor/parkingsensor.csv'
-    # read_file = s3_resource.Object(bucket, key).get()
+    # df = pd.read_csv('flaskr/parking_sensor/data/parkingsensor.csv', parse_dates=True, infer_datetime_format=True)
+    # return df
+    bucket = 'opendataplayground.deakin'
+    # get existing dataframe from csv on S3
+    s3_resource = boto3.resource('s3')
 
-    # # load data from csv file
-    # return pd.read_csv(read_file['Body'], parse_dates=True, infer_datetime_format=True)
+    df = None
+    now = datetime.today().replace(microsecond=0)
+
+    num_days = 28
+    keys = [f'parkingsensor/daily/{now.date() - timedelta(days=i)}.csv' for i in range(num_days)]
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_days) as executor:
+        for reader in executor.map(get(bucket, s3_resource), keys):
+            day_file = pd.read_csv(reader['Body'])
+            df = day_file if df is None else df.append(day_file)
+
+    return df
