27ee6f0a765fb8f7fe136ab0c752d934063ffe0c
sansomguy <jsansomsherwill@deakin.edu.au>
2021-09-22 21:31:25 +1000

    Reverted some changes that were not working too well
    Former-commit-id: b5513db26edd8761d938f6f242915f7cd6dc0dd5 [formerly 15f00f4b1fe29c5f4004c3f466c4b8247479eb2a]
    Former-commit-id: 4f68e1b46207db1e77e40a4dbb1df36f71d7f8e4

--- T2_2021/webapp/flaskr/parking_sensor/parking_sensor_data.py
+++ T2_2021/webapp/flaskr/parking_sensor/parking_sensor_data.py
@@ -1,35 +1,39 @@
 import boto3
+import botocore
 import pandas as pd
 import concurrent.futures
 from datetime import datetime, timedelta
 import threading
 
-thread_local = threading.local()
+def get():
+    bucket = 'opendataplayground.deakin'
+    client_config = botocore.config.Config(
+        max_pool_connections=50,    
+    )
+    s3_client = boto3.client('s3', region_name="ap-southeast-2", config=client_config)
+    def getter(key):
+        return s3_client.get_object(Bucket=bucket, Key=key)
 
-def get(bucket, resource):
-    
-    def get_key(key):
-        thread_local.reader = resource.Object(bucket, key).get()
-        return thread_local.reader
-
-    return get_key
+    return getter
 
 ''' Returns Parking sensor csv'''
 def get_parking_sensor_data():
     # df = pd.read_csv('flaskr/parking_sensor/data/parkingsensor.csv', parse_dates=True, infer_datetime_format=True)
     # return df
-    bucket = 'opendataplayground.deakin'
-    # get existing dataframe from csv on S3
-    s3_resource = boto3.resource('s3')
 
     df = None
-    now = datetime.today().replace(microsecond=0)
+    # now = datetime.today().replace(microsecond=0)
 
-    num_days = 28
-    keys = [f'parkingsensor/daily/{now.date() - timedelta(days=i)}.csv' for i in range(num_days)]
-    with concurrent.futures.ThreadPoolExecutor(max_workers=num_days) as executor:
-        for reader in executor.map(get(bucket, s3_resource), keys):
-            day_file = pd.read_csv(reader['Body'])
-            df = day_file if df is None else df.append(day_file)
+    # num_days = 28
+    # keys = [f'parkingsensor/daily/{now.date() - timedelta(days=i)}.csv' for i in range(num_days)]
+    # # with concurrent.futures.ThreadPoolExecutor(max_workers=num_days) as executor:
+    # getter = get()
+    # for key in keys:
+    #     reader = getter(key)
+    #     day_file = pd.read_csv(reader['Body'])
+    #     df = day_file if df is None else df.append(day_file)
+    getter = get()
+    reader = getter('parkingsensor/parkingsensor.csv')
+    df = pd.read_csv(reader['Body'], parse_dates=True, infer_datetime_format=True)
 
     return df
