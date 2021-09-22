bcb3a9e781b1a8b8d4c3560f84415d952bc2c3f0
sansomguy <jsansomsherwill@deakin.edu.au>
2021-09-23 09:32:40 +1000

    Fixed some small details with respect to styling and visualizations
    Former-commit-id: ab835969dfee290d5ee7edf0a65a1d7699796f97 [formerly e706fff4a30b7b04c6eebda9867f5d911b2870b3]
    Former-commit-id: d5e2ed01bb864d8ab6085ec74ef46997c9a308f7

--- T2_2021/webapp/flaskr/static/playground.js
+++ T2_2021/webapp/flaskr/static/playground.js
@@ -100,13 +100,13 @@ function addLayer(map, features, markerName) {
         'layout': {
             'icon-image': markerName,
             // get the title name from the source's "title" property
-            'text-field': ['get', 'title'],
-            'text-font': [
-                'Open Sans Semibold',
-                'Arial Unicode MS Bold'
-            ],
-            'text-offset': [0, 1.25],
-            'text-anchor': 'top'
+            // 'text-field': ['get', 'title'],
+            // 'text-font': [
+            //     'Open Sans Semibold',
+            //     'Arial Unicode MS Bold'
+            // ],
+            // 'text-offset': [0, 1.25],
+            // 'text-anchor': 'top'
         }
     });
 }
