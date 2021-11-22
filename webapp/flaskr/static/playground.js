d37c796825bbb5dc5a9195ea350598a2ffb0990b
Joshua Sansom <jsansomsherwill@deakin.edu.au>
2021-11-22 14:08:04 +1100

    Reduced the duplicate code for loading parking availability tool. Code is now re-used for easier maintenance
    Former-commit-id: 777e550375d02f2f0bd5c69e84d64aaf8f775d42 [formerly e32df779b1e19deb915dedc8b3bf4bbef58802fb]
    Former-commit-id: 72181083a910a71adf1b00a75b24922b28f238a5

--- webapp/flaskr/static/playground.js
+++ webapp/flaskr/static/playground.js
@@ -1,5 +1,3 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoianNhbnNvbXNoZXJ3aWxsIiwiYSI6ImNrc2Y4Z255MjE4NXAydXBpbGs5bHlha3IifQ.dgv4c4Gl2v_K0TN_RpYfKg';
-
 function setupParkingSensorStep() {
     return new Promise((resolve, _) => {
         const map = new mapboxgl.Map({
@@ -16,105 +14,13 @@ function setupParkingSensorStep() {
     })
 }
 
-var unknownMarkerName = 'unknown_marker'
-var presentMarkerName = 'occupied_marker'
-var unoccupiedMarkerName = 'unoccupied_marker'
-
-async function showParkingSensorsOnMap(map) {
-    // add the markers for different statuses to our available images
-    let imagesPromise = loadMarkers(map)
-    let latestSensors = fetch($SCRIPT_ROOT + "/playground/parking-sensors/latest.json")
-        .then(result => result.json())
-
-    let [_, data] = await Promise.all([imagesPromise, latestSensors])
-    // convert sensor information into geojson
-    let features = data
-        .reduce((features, parkingSensor) => {
-            let { lat, lon, status } = parkingSensor
-            let lng = lon
-            let feature = {
-
-                'type': 'Feature',
-                'geometry': {
-                    'type': 'Point',
-                    'coordinates': [
-                        lng, lat
-                    ]
-                },
-                'properties': {
-                    'title': status
-                }
-            }
-            // add next parking sensor to the geojson features
-            // with respect to status
-            features[status].push(feature)
-            return features
-        }, { 'Present': [], 'Unoccupied': [], 'Unknown': [] })
-
-    // add each status type to the map
-    // as new marker mapbox layer
-    let { Present, Unoccupied, Unknown } = features
-
-    addLayer(map, Unknown, unknownMarkerName)
-    addLayer(map, Present, presentMarkerName)
-    addLayer(map, Unoccupied, unoccupiedMarkerName)
-}
-
-function loadMapImage(map, image) {
-    return new Promise((resolve, reject) => map.loadImage($SCRIPT_ROOT + image,
-        (error, image) => {
-            if (error)
-                reject(error)
-            else
-                resolve(image)
-        }))
-}
-
-function loadMarkers(map) {
-    return Promise.all([
-        loadMapImage(map, '/static/occupied_parking_sensor_25px.png'), // url of custom png markers to represent status
-        loadMapImage(map, '/static/unoccupied_parking_sensor_25px.png'),
-        loadMapImage(map, '/static/unknown_parking_sensor_25px.png')
-    ]).then(([occupied, unoccupied, unknown]) => {
-        map.addImage(presentMarkerName, occupied)
-        map.addImage(unoccupiedMarkerName, unoccupied)
-        map.addImage(unknownMarkerName, unknown)
-    })
-}
-
-function addLayer(map, features, markerName) {
-    map.addSource(markerName, {
-        'type': 'geojson',
-        'data': {
-            'type': 'FeatureCollection',
-            'features': features
-        }
-    });
-    map.addLayer({
-        'id': markerName,
-        'type': 'symbol',
-        'source': markerName,
-        'layout': {
-            'icon-image': markerName,
-            // get the title name from the source's "title" property
-            // 'text-field': ['get', 'title'],
-            // 'text-font': [
-            //     'Open Sans Semibold',
-            //     'Arial Unicode MS Bold'
-            // ],
-            // 'text-offset': [0, 1.25],
-            // 'text-anchor': 'top'
-        }
-    });
-}
-
 function delete_placeholder(id) {
     document.getElementById(id).remove()
 }
 
 window.addEventListener('load', async () => {
     // Load the maps in the order in which they are rendered on the playground screen
-    await setupParkingAvailabilityMap('solutionMap', '.solution_demo')
+    await setupParkingAvailabilityMap('solution_demo')
     await setupParkingSensorStep()
-    await setupParkingAvailabilityMap('toolMap', '.final_step')
+    await setupParkingAvailabilityMap('final_step')
 })
