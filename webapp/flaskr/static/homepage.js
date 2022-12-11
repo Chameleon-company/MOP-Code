4e63a5c7dd5a6de2b11c198e054e89c2ac8d6fc8
Caleb <webbca@deakin.edu.au>
2022-12-11 21:09:56 +1100

    Updated dataset imports

--- webapp/flaskr/static/homepage.js
+++ webapp/flaskr/static/homepage.js
@@ -1,34 +1,38 @@
-// First, define the template for the new rows as a string
 var rowTemplate = "<tr><td>{{name}}</td><td>{{difficulty}}</td></tr>";
 
-// Next, define a function for generating a new row based on the template
-// and some provided data
 function createNewRow(name, difficulty) {
-  // Replace the placeholders in the template with the actual data
   return rowTemplate.replace("{{name}}", name)
                     .replace("{{difficulty}}", difficulty);
 }
 
 function addUseCases() {
-    var table = document.getElementById("use-case-table");
+    let useCaseTable = document.getElementById("use-case-table");
     fetch(`${$SCRIPT_ROOT}/static/search.json`)
         .then((response) => response.json())
         .then ((data) => {
             for (item in data) {
-                table.innerHTML += createNewRow(data[item].title,data[item].difficulty);
+                useCaseTable.innerHTML += createNewRow(data[item].title,data[item].difficulty);
             }
         });
 }
 
 function addDatasets() {
-    var table = document.getElementById("dataset-table");
-    fetch(`${$SCRIPT_ROOT}/search/datasets?query=${searchTerms.join(' ')}`)
-        .then(result => result.json())
-        .then ((datasets) => {
-            for (item in datasets) {
-                console.log(datasets)
+    let datasetTable = document.getElementById("dataset-table");
+    fetch(`${$SCRIPT_ROOT}/search/datasets?query`)
+        .then((response) => response.json())
+        .then((data) => {
+            for (item in data) {
+                let datasetName = data[item].Name
+                let datasetDownloads = data[item].Downloads
+                let datasetURL = data[item].Permalink
+                if (datasetName.indexOf("(") > -1) {
+                    datasetTable.innerHTML += createNewRow(datasetName.substring(0,datasetName.indexOf("(")), datasetDownloads)
+                } else {
+                    datasetTable.innerHTML += createNewRow(datasetName, datasetDownloads)
+                }
             }
         })
 }
 
-addUseCases()
+addUseCases();
+addDatasets();
