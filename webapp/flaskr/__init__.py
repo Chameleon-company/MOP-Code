acdb17f500dee45e1c316631b68f520dc888003b
lkocon69 <103554845+lkocon69@users.noreply.github.com>
2023-05-09 03:01:07 -0400

    Updated CSP
    The Content Security Policy has been updated to include the product's current resources being used. This should cut down a lot of false positives that are shown in CSP reporting. The Content Security Policy has been flagged as vulnerable in scans.
    
    To make sure the CSP works for the product, we should verify that the product is actually reporting CSP warnings and see it block unrecognised resources (perhaps when the Contact page has been implemented)

--- webapp/flaskr/__init__.py
+++ webapp/flaskr/__init__.py
@@ -11,7 +11,60 @@ def create_app(test_config=None):
     # In particular, this sets the 'x-frame-options=SAMEORIGIN' flag in the HTTP response header to prevent clickjacking.
     # The 'content_security_policy' argument is set to allow content from anywhere or it is too restrictive.
     Talisman(app,
-             content_security_policy = {'default-src': '*'},
+             content_security_policy = {'default-src' : ['\'self\''
+             ,'stamen-tiles.a.ssl.fastly.net'
+             ,'*.stamen-tiles.a.ssl.fastly.net'
+             ,'*.tile.openstreetmap.org'
+             ,'*.mapbox.com'
+             ,'*.cdnjs.cloudflare.com'
+             ]
+             ,'style-src' : 
+             ['\'self\''
+             ,'cdn.jsdelivr.net'
+             ,'*.cdn.jsdelivr.net'
+             ##,'\'unsafe-inline\''
+             ,'maxcdn.bootstrapcdn.com'
+             ,'*.maxcdn.bootstrapcdn.com'
+             ,'cdnjs.cloudflare.com'
+             ,'*.cdnjs.cloudflare.com'
+             ,'*.mapbox.com'
+             ,'rawcdn.githack.com'
+             ]
+             ,'script-src': 
+             ['\'self\''
+             ,'maxcdn.bootstrapcdn.com'
+             ,'*.maxcdn.bootstrapcdn.com'
+             ,'cdnjs.cloudflare.com'
+             ,'*.cdnjs.cloudflare.com'
+             ##,'\'unsafe-inline\''
+             ,'cdn.jsdelivr.net/npm/'
+             ,'*.cdn.jsdelivr.net/npm/'
+             ##,'\'unsafe-eval\''
+             ,'https://code.jquery.com/jquery-1.12.4.min.js'
+             ,'unpkg.com'
+             ,'*.unpkg.com'
+             ,'*.mapbox.com'
+             ,'https://leaflet.github.io/Leaflet.heat/dist/leaflet-heat.js'
+             ]
+             ,'font-src' : ['\'self\''
+             ,'cdnjs.cloudflare.com'
+             ,'*.cdnjs.cloudflare.com'
+             ,'maxcdn.bootstrapcdn.com'
+             ,'*.maxcdn.bootstrapcdn.com'
+             ]
+             ,'img-src' : ['\'self\''
+             ,'cdn.jsdelivr.net'
+             ,'*.cdn.jsdelivr.net'
+             ,'global.ssl.fastly.net/light_all/'
+             ,'*.global.ssl.fastly.net/light_all/'
+             ,'*.tile.openstreetmap.org'
+             ,'*.cdnjs.cloudflare.com'
+             ,'cdnjs.cloudflare.com'
+             ,'cartodb-basemaps-b.global.ssl.fastly.net'
+             ,'*.cartodb-basemaps-b.global.ssl.fastly.net'
+             ,'*.mapbox.com'
+             ]
+             },
              content_security_policy_report_only = True,
              content_security_policy_report_uri = '/tools/csp-report')
     app.config.from_mapping(
