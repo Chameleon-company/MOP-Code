0d7b6e46024d9bb2ffbad88757a659c3d07de43c
jiahaopan-Deakin <jiahaopan814@gmail.com>
2023-09-22 16:55:27 +1000

    Fix up nav bugs in landing page and create a new page called data which store all user case pages

--- webapp/flaskr/controllers/use_cases.py
+++ webapp/flaskr/controllers/use_cases.py
@@ -1,5 +1,5 @@
 from markupsafe import escape
-from flask import Blueprint, jsonify, render_template, request
+from flask import Blueprint, jsonify, render_template, request, send_from_directory
 
 from flaskr.logic.parking_availability.view_model import build_view_model
 
@@ -8,6 +8,11 @@ bp = Blueprint('use_cases', __name__, url_prefix='/use-cases')
 @bp.route("/<name>", methods=('GET',))
 def use_cases(name):
     return render_template(f"use-cases/{escape(name)}.html", view_model = get_view_model(name))
+
+@bp.route("/ajax/<name>", methods=('GET',))
+def use_cases_ajax(name):
+    view_model = get_view_model(name)
+    return jsonify(view_model)
 
 """
     get the view model associated with the use-case name
