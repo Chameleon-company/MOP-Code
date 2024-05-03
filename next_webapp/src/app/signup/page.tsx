31ba4fa4789ba1e6050ea07c8d44cb60b1f3f472
Tyrepo1 <dimonkey1912@gmail.com>
2024-05-03 14:38:14 +1000

    Added accesibility features for Footer and Signup page

--- next_webapp/src/app/signup/page.tsx
+++ next_webapp/src/app/signup/page.tsx
@@ -5,85 +5,81 @@ import React, { useState } from 'react';
 import '../../../public/styles/signup.css';
 
 const SignUpPage = () => {
-  const [password, setPassword] = useState('');
-  const [passwordStrength, setPasswordStrength] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordStrength, setPasswordStrength] = useState('');
 
-  const handlePasswordChange = (event) => {
-      const newPassword = event.target.value;
-      setPassword(newPassword);
-      setPasswordStrength(checkPasswordStrength(newPassword));
-  };
+    const handlePasswordChange = (event) => {
+        const newPassword = event.target.value;
+        setPassword(newPassword);
+        setPasswordStrength(checkPasswordStrength(newPassword));
+    };
 
-  const checkPasswordStrength = (password) => {
-      if (password.length < 6) {
-          return 'Weak';
-      } else if (password.length < 10) {
-          return 'Moderate';
-      } else {
-          return 'Strong';
-      }
-  };
+    const checkPasswordStrength = (password) => {
+        if (password.length < 6) {
+            return 'Weak';
+        } else if (password.length < 10) {
+            return 'Moderate';
+        } else {
+            return 'Strong';
+        }
+    };
 
-  const getPasswordStrengthColor = () => {
-      switch (passwordStrength) {
-          case 'Weak':
-              return 'red';
-          case 'Moderate':
-              return 'orange';
-          case 'Strong':
-              return 'green';
-          default:
-              return 'transparent';
-      }
-  };
-  
+    const getPasswordStrengthColor = () => {
+        switch (passwordStrength) {
+            case 'Weak':
+                return 'red';
+            case 'Moderate':
+                return 'orange';
+            case 'Strong':
+                return 'green';
+            default:
+                return 'transparent';
+        }
+    };
+    // Calculate the width of the password strength bar based on the length of the password (limited to 10 characters)
+    const passwordStrengthStyle = {
+        width: `${Math.min((password.length / 10) * 100, 100)}%`,
+        backgroundColor: getPasswordStrengthColor(),
+    };
 
-  // Calculate the width of the password strength bar based on the length of the password (limited to 10 characters)
-  const passwordStrengthStyle = {
-    width: `${Math.min((password.length / 10) * 100, 100)}%`,
-    backgroundColor: getPasswordStrengthColor(),
-};
-
-  return (
-      <div className="flex flex-col items-center justify-center min-h-screen">
-          {/* Already a member section */}
-          <div className="already-member">
-              <p className="text-sm mr-2">Already a member?</p>
-              <a href="/login" className="login-button">Log In</a>
-          </div>
-
-          {/* Logo */}
-          <div className="logo">
-              {/* Add your logo here */}
-              <img src="/img/image.png" alt="Logo" />
-          </div>
-
-          {/* Sign-up form */}
-          <div className="signup-form">
-              <div className="text-center mb-8">
-                  <h2 className="text-2xl">Account Sign Up</h2>
-              </div>
-              <div className="mb-4 flex">
-                  <input type="text" placeholder="First name" className="first-name" />
-                  <input type="text" placeholder="Last name" className="last-name" />
-              </div>
-              <div className="mb-4">
-                  <input type="email" placeholder="Email" className="email" />
-              </div>
-              <div className="mb-4">
-                  <input type="password" placeholder="Password" className="password" onChange={handlePasswordChange} />
-              </div>
-              <div className="mb-4">
-                  {/* Password strength bar */}
-                  <div className="password-strength-bar">
-                      <div className="password-strength-bar-inner" style={passwordStrengthStyle}></div>
-                  </div>
-                  <p className="password-strength-text">Password Strength: {passwordStrength}</p>
-              </div>
-              <button className="next-button">Next</button>
-          </div>
-      </div>
-  );
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <div className="already-member">
+                {/* Already a member section */}
+                <p className="text-sm mr-2">Already a member?</p>
+                <a href="/login" className="login-button">Log In</a>
+            </div>
+            {/* Logo */}
+            <div className="logo">
+                {/* Add your logo here */}
+                <img src="/img/image.png" alt="Logo" />
+            </div>
+            {/* Sign-up form */}
+            <div className="signup-form">
+                <div className="text-center mb-8">
+                    <h2 className="text-2xl">Account Sign Up</h2>
+                </div>
+                <div className="mb-4 flex">
+                    <input type="text" placeholder="First name" className="first-name" id="firstName" />
+                    <input type="text" placeholder="Last name" className="last-name" id="lastName" />
+                </div>
+                <div className="mb-4">
+                    <input type="email" placeholder="Email" className="email" id="email" />
+                </div>
+                <div className="mb-4">
+                    <input type="password" placeholder="Password" className="password" onChange={handlePasswordChange} id="password" />
+                </div>
+                <div className="mb-4">
+                    {/* Password strength bar */}
+                    <div className="password-strength-bar">
+                        <div className="password-strength-bar-inner" style={passwordStrengthStyle}></div>
+                    </div>
+                    <p className="password-strength-text">Password Strength: {passwordStrength}</p>
+                </div>
+                <button className="next-button">Next</button>
+            </div>
+        </div>
+    );
 };
 
 export default SignUpPage;
--- next_webapp/src/components/Footer.tsx
+++ next_webapp/src/components/Footer.tsx
@@ -1,27 +1,25 @@
-// Footer.js
 import React from 'react';
 
 const Footer = () => {
     return (
-        <footer class="bg-white text-black justify-center">
-            <div class="w-full px-32 py-7 flex items-center">
-                <div class="flex-shrink-0 mr-4">
-                    <img src='/img/header-logo.png' alt='logo' class='w-20' />
+        <footer className="bg-white text-black justify-center">
+            <div className="w-full px-32 py-7 flex items-center">
+                <div className="flex-shrink-0 mr-4">
+                    <img src='/img/header-logo.png' alt='Logo' className='w-20' />
                 </div>
-                <div class="flex-grow text-center">
-                    <ul class="flex justify-center gap-20">
-                        <a href='/privacypolicy'><li>Privacy Policy</li></a>
-                        <a href='/licensing'><li>Licensing</li></a>
-                        <a href='/contact'><li>Contact Us</li></a>
+                <div className="flex-grow text-center">
+                    <ul className="flex justify-center gap-20">
+                        <li><a href='/privacypolicy'>Privacy Policy</a></li>
+                        <li><a href='/licensing'>Licensing</a></li>
+                        <li><a href='/contact'>Contact Us</a></li>
                     </ul>
                 </div>
-                <div class="flex-shrink-0 ml-4 flex gap-2">
+                <div className="flex-shrink-0 ml-4 flex gap-2">
                     <a href='https://data.melbourne.vic.gov.au/pages/home/'>Melbourne Open Data</a>
-                    <img src='https://www.svgrepo.com/show/510970/external-link.svg' alt='link icon' class='w-5' />
+                    <img src='https://www.svgrepo.com/show/510970/external-link.svg' alt='Link Icon' className='w-5' />
                 </div>
             </div>
         </footer>
-
     );
 };
 
