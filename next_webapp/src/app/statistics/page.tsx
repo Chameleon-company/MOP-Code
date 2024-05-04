36b5321052535313a89849e0a6205cc09b525db9
Yhxiao <xiao2364390271@icloud.com>
2024-05-04 20:24:52 +1000

    StatisticPage_UI_DEV_VictorXiao_#PR3
    Statistics page UI -development] https://trello.com/c/2OEiIsAi/541-statistics-page-ui-development-max-2
    
    Created Barplot for Popularity Range
    Created filter drop list for years
    Created filter drop list for trimesters
    Fixed the issue that search function, now allowing users to search regardless of the case used.

--- next_webapp/src/app/statistics/page.tsx
+++ next_webapp/src/app/statistics/page.tsx
@@ -1,28 +1,86 @@
 "use client"
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import { set } from 'firebase/database';
 //import caseStudies from './database';  // Adjust the path according to your project structure
 
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from "chart.js";
+  import { Bar } from "react-chartjs-2";
+  
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+  
 
 const Statistics = () => {
     // Dummy Array
     const caseStudies = [
-        { id: 1, tag: 'Safety and Well-being', publishNumber: '4', popularity: '11%'},
-        { id: 2, tag: 'Environment and Sustainability', publishNumber: '5', popularity: '20%'},
-        { id: 3, tag: 'Business and activity', publishNumber: '8', popularity: '90%'}
+        { id: 1, tag: 'Safety and Well-being', publishNumber: '4', popularity: '11%', trimester: '1', year: "2023"},
+        { id: 2, tag: 'Environment and Sustainability', publishNumber: '5', popularity: '20%',trimester: '2', year: "2023"},
+        { id: 3, tag: 'Business and activity', publishNumber: '8', popularity: '90%',trimester: '3', year: "2023"},
+        { id: 4, tag: 'Safety and Well-being', publishNumber: '4', popularity: '11%',trimester:'2', year: "2023"},
+        { id: 5, tag: 'Environment and Sustainability', publishNumber: '5', popularity: '20%',trimester:'3', year: "2023"},
+        { id: 6, tag: 'Business and activity', publishNumber: '8', popularity: '90%',trimester:'2', year: "2023"},
+        { id: 7, tag: 'Safety and Well-being', publishNumber: '4', popularity: '11%',trimester:'2', year: "2023"},
+        { id: 8, tag: 'Environment and Sustainability', publishNumber: '5', popularity: '20%',trimester:'1', year: "2023"},
+        { id: 9, tag: 'Business and activity', publishNumber: '8', popularity: '90%',trimester:'2', year: "2023"}
         
     ];
-    
+     
+
 
     // State for storing the filtered results and all filters
     const [filteredStudies, setFilteredStudies] = useState(caseStudies);
     const [tagFilter, setTagFilter] = useState('');
     const [publishFilter, setPublishFilter] = useState('');
     const [popularityFilter, setPopularityFilter] = useState('');
+    const [pagefilter, setPageFilter] = useState('5');
+    const [search, setSearchTerm] = useState('');
+    const [yearFilter, setYearFilter] = useState('');
+    const [trimesterFilter, setTrimesterFilter] = useState('');
+
+    
 
     // Distinct tags for the dropdown
-    const tags = Array.from(new Set(caseStudies.map(study => study.tag)));
+    const tags = useMemo(() => Array.from(new Set(caseStudies.map(study => study.tag))), [caseStudies]);
+    const years = useMemo(() => Array.from(new Set(caseStudies.map(study => study.year))), [caseStudies]);
+    const trimesters = useMemo(() => ['1', '2', '3'], []);
+
+
+
+    const popularityByTag = useMemo(() => {
+        const sumPopularity: {[key: string]: number} = {};
+        const tagCounts: {[key: string]: number} = {};
+        caseStudies.forEach(study => {
+            const popularity = parseFloat(study.popularity.replace('%', ''));
+            if (sumPopularity[study.tag]) {
+                sumPopularity[study.tag] += popularity;
+                tagCounts[study.tag] += 1;
+            } else {
+                sumPopularity[study.tag] = popularity;
+                tagCounts[study.tag] = 1;
+            }
+        });
+
+        return Object.keys(sumPopularity).map(tag => ({
+            tag: tag,
+            avgPopularity: sumPopularity[tag] / tagCounts[tag]
+        }));
+    }, [caseStudies]);
 
     // Effect to handle filtering based on tag, publish number, and popularity
     useEffect(() => {
@@ -64,13 +122,85 @@ const Statistics = () => {
                 }
             });
         }
+        if (yearFilter) {
+            filtered = filtered.filter(study => study.year === yearFilter);
+        }
+        if (trimesterFilter) {
+            filtered = filtered.filter(study => study.trimester === trimesterFilter);
+        }
         setFilteredStudies(filtered);
         
-    }, [tagFilter, publishFilter, popularityFilter]);
+    }, [tagFilter, publishFilter, popularityFilter, yearFilter, trimesterFilter]);
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const recordsPage = parseInt(pagefilter);
+    const lastindex = currentPage * recordsPage;
+    const firstindex = lastindex - recordsPage;
+    const records = filteredStudies.slice(firstindex, lastindex);
+    const npage = Math.ceil(filteredStudies.length/recordsPage);
+   // const numbers = [...Array(npage + 1).keys()].slice(1);
+
+   // Counting the values of trimester to plot on the graph
+    const tri1 = caseStudies.filter(item => item.trimester== '1').length;
+    const tri2 = caseStudies.filter(item => item.trimester== '2').length;
+    const tri3 = caseStudies.filter(item => item.trimester== '3').length
+
+    // Store the reqired variable for plotting the graph
+   const data = {
+    labels: ['Trimester 1', 'Triemster 2', 'Trimester 3'],
+    datasets: [
+      {
+        label: 'Data Series 1',
+        backgroundColor: 'rgba(75, 192, 192, 0.6)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1,
+        data: [tri1, tri2,tri3 ],
+      },
+      
+    ],
+  };
+
+  const data2 = {
+    labels: popularityByTag.map(item => item.tag),
+    datasets: [
+        {
+            label: 'Average Popularity',
+            backgroundColor: 'rgba(255, 99, 132, 0.6)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+            data: popularityByTag.map(item => item.avgPopularity),
+        },
+    ],
+    };
+
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+        maintainAspectRatio: false ,
+      },
+    },
+  };
 
     return (
         <div style={{ margin: '0 auto', minHeight: '100vh', display: 'flex', flexDirection: 'column' }} className="font-sans bg-gray-100 text-black">
             <Header />
+            <h1 className='text-7xl font-bold px-[2rem] pt-[1rem] pb-[4rem]'> Statistics </h1>
+                <div className='flex '>
+                    <div className='bg-white shadow-2xl ml-[10rem] h-[22rem] w-[40rem] mb-[5rem] pb-[10rem]'>
+                        <h4 className='m-10 font-bold text-[25px]'>Trimester which published the most test cases </h4>
+                        <div className='mx-5'>
+                        <Bar data={data} height={"25%"} width={"90%"}options={options}/>
+                        </div>
+                    </div>
+                    <div className='bg-white shadow-2xl ml-[10rem] h-[22rem] w-[40rem] mb-[5rem] pb-[10rem]'>
+                        <h4 className='m-10 font-bold text-[25px]'>Popularity by Tag</h4>
+                        <div className='mx-5'>
+                        <Bar data={data2} height={"25%"} width={"90%"}options={options}/>
+                        </div>
+                    </div>
+                </div>
             <main style={{ flex: '1 0 auto', width: '100%' }}>
                 <div style={{padding: '0 50px' }}>
                     
@@ -78,7 +208,7 @@ const Statistics = () => {
                         <select 
                             value={tagFilter} 
                             onChange={(e) => setTagFilter(e.target.value)} 
-                            className="p-2 m-2 border"
+                            className="p-2 m-2 border shadow-lg"
                         >
                             <option value="">All Tags</option>
                             {tags.map(tag => (
@@ -90,7 +220,7 @@ const Statistics = () => {
                         <select
                             value={publishFilter}
                             onChange={(e) => setPublishFilter(e.target.value)}
-                            className="p-2 m-2 border"
+                            className="p-2 m-2 border shadow-lg"
                         >
                             <option value="">All Publish Numbers</option>
                             <option value="lessThan4">Less than 4</option>
@@ -100,7 +230,7 @@ const Statistics = () => {
                         <select
                             value={popularityFilter}
                             onChange={(e) => setPopularityFilter(e.target.value)}
-                            className="p-2 m-2 border"
+                            className="p-2 m-2 border shadow-lg"
                         >
                             <option value="">All Popularity Ranges</option>
                             <option value="0to20">0 - 20%</option>
@@ -109,7 +239,39 @@ const Statistics = () => {
                             <option value="60to80">60 - 80%</option>
                             <option value="80to100">80 - 100%</option>
                         </select>
-                        <div className="overflow-hidden rounded-lg shadow">
+
+                        <select value={yearFilter} onChange={(e) => setYearFilter(e.target.value)} className="p-2 m-2 border shadow-lg">
+                            <option value="">All Years</option>
+                              {years.map(year => (
+                            <option key={year} value={year}>{year}</option>
+                            ))}
+                        </select>
+
+                        <select value={trimesterFilter} onChange={(e) => setTrimesterFilter(e.target.value)} className="p-2 m-2 border shadow-lg">
+                            <option value="">All Trimesters</option>
+                              {trimesters.map(trimester => (
+                            <option key={trimester} value={trimester}>{'Trimester ' + trimester}</option>
+                            ))}
+                        </select>
+                        
+                        <div className='flex'>
+                                <div className='border-solid bg-white shadow-2xl border-2 border-black-600 py-8 px-10 my-10'>
+                                <h2 className=' text-2xl font-bold text-gray-400 '>Total Results</h2>
+                                <p className='text-[1.8rem] font-bold text-center pt-[15px] px-2rem font-bold text-black-400 '>{filteredStudies.length}</p>
+                                </div>
+                                <div>
+                                    <p></p>
+                                </div>
+                            </div>
+                        <div className="overflow-hidden rounded-lg shadow">    
+                        <form className="flex items-center w-full">
+                        <input
+                              type="search"
+                                 placeholder="Enter Tag name"
+                                 className="w-full px-4 py-2 mr-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-green-500"
+                                 onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
+                            />
+                        </form>
                             <table className="min-w-full bg-white">
                                 <thead>
                                     <tr>
@@ -125,10 +287,18 @@ const Statistics = () => {
                                         <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider rounded-tr-lg">
                                             Popularity
                                         </th>
+                                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                            Year
+                                        </th>
+                                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider rounded-tr-lg">
+                                            Trimester
+                                        </th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {filteredStudies.map((study, index) => (
+                                    {records.filter((item)=>{
+                                        return search.toLowerCase() === ''? item : item.tag.toLowerCase().includes(search);
+                                    }).map((study, index) => (
                                         <tr key={study.id} className={index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}>
                                             <td className="px-5 py-5 border-b border-gray-200 text-sm">
                                                 {study.id}
@@ -142,17 +312,70 @@ const Statistics = () => {
                                             <td className="px-5 py-5 border-b border-gray-200 text-sm">
                                                 {study.popularity}
                                             </td>
+                                            <td className="px-5 py-5 border-b border-gray-200 text-sm">
+                                                {study.year}
+                                            </td>
+                                            <td className="px-5 py-5 border-b border-gray-200 text-sm">
+                                                {study.trimester}
+                                            </td>
                                         </tr>
                                     ))}
                                 </tbody>
                             </table>
                         </div>
+                        <nav className='bg-gray-200 py-3'>
+                            <ul className = 'pagei'>
+                            <li className='page-item'>
+                                <button className="  float-right  text-black font-bold py-1 px-2 "><a href='#' className='page-link' onClick={nextPage}>{'>'}</a></button>
+                                <p className='float-right text-black py-1 px-2'>{currentPage}</p>
+                                </li>
+                                <li className='page-item'>
+                                    <button className=" float-right  text-black font-bold py-1 px-2 " >
+                                    <a href='#' className='page-link' onClick={prePage}>{'<'}</a>
+                                    </button>
+                                </li>
+                               
+                                <li className='float-right py-2'>
+                                    <select value={pagefilter} 
+                                     onChange={(e) => setPageFilter(e.target.value)} 
+                                    >
+                                        <option value="5">5</option>
+                                        <option value="10">10</option>
+                                        <option value="20">20</option>
+                                        <option value="30">30</option>
+                                    </select>
+                                </li>
+                                <li  className='float-right'><p className='px-3 py-1'>Rows per page</p></li>
+                                <li>
+                                <p>{firstindex+1} - {lastindex} of {filteredStudies.length}</p>
+                                </li>
+                               
+                            </ul>
+                        </nav>
                     </section>
                 </div>
             </main>
             <Footer />
         </div>
     );
+
+    function prePage(){
+        if(currentPage !== 1){
+            setCurrentPage(currentPage - 1)
+        }
+        else{
+            setCurrentPage(1);
+        }
+    }
+
+    function nextPage(){
+        if(currentPage !== npage){
+            setCurrentPage(currentPage + 1);
+        }
+        else{
+            setCurrentPage(npage);
+        }
+    }
 };
 
 export default Statistics;
