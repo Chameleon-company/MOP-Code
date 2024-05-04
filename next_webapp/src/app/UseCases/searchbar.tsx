75acba44d40f8f6dd30d008cb826977fc65d6878
Kasey <kaseywu130@gmail.com>
2024-05-04 20:05:50 +1000

    WD_Patch_fix searchMode filter
    Fixed the issue that filter result doesn't change when clicking.

--- next_webapp/src/app/UseCases/searchbar.tsx
+++ next_webapp/src/app/UseCases/searchbar.tsx
@@ -21,7 +21,10 @@ const SearchBar = ({
 
   return (
     <div className="p-4 flex flex-col pl-0 ml-0">
-      <form onSubmit={handleSubmit} className="flex items-center w-full max-w-10xl space-x-3">
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center w-full max-w-10xl space-x-3"
+      >
         <input
           type="search"
           placeholder="Case study name or category"
@@ -29,28 +32,30 @@ const SearchBar = ({
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <label htmlFor="category-select" className="sr-only">All categories</label>
+        <label htmlFor="category-select" className="sr-only">
+          All categories
+        </label>
         <div className="flex-shrink-0">
-        <select
-          id="category-select"
-          className="text-black border-2 border-gray-300 border-l-1 px-4 py-2 focus:outline-none focus:border-green-500 rounded-l-md rounded-r-lg"
-          value={category}
-          onChange={(e) => setSearchMode(e.target.value as SEARCH_MODE)}
-        >
-          <option value={SEARCH_MODE.TITLE}>Search by title</option>
-          <option value={SEARCH_MODE.CONTENT}>Search by content</option>
-        </select>
-        <select
-          className="text-black mr-3 border-2 border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:border-green-500"
-          value={category}
-          onChange={(e) => setCategory(e.target.value as CATEGORY)}
-        >
-          <option value={CATEGORY.ALL}>All categories</option>
-          {/* Add more options here based on your categories */}
-          <option value={CATEGORY.INTERNET}>Internet</option>
-          <option value={CATEGORY.EV}>EV</option>
-          <option value={CATEGORY.SECURITY}>Security</option>
-        </select>
+          <select
+            id="category-select"
+            className="text-black border-2 border-gray-300 border-l-1 px-4 py-2 focus:outline-none focus:border-green-500 rounded-l-md rounded-r-lg"
+            value={searchMode}
+            onChange={(e) => setSearchMode(e.target.value as SEARCH_MODE)}
+          >
+            <option value={SEARCH_MODE.TITLE}>Search by title</option>
+            <option value={SEARCH_MODE.CONTENT}>Search by content</option>
+          </select>
+          <select
+            className="text-black mr-3 border-2 border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:border-green-500"
+            value={category}
+            onChange={(e) => setCategory(e.target.value as CATEGORY)}
+          >
+            <option value={CATEGORY.ALL}>All categories</option>
+            {/* Add more options here based on your categories */}
+            <option value={CATEGORY.INTERNET}>Internet</option>
+            <option value={CATEGORY.EV}>EV</option>
+            <option value={CATEGORY.SECURITY}>Security</option>
+          </select>
         </div>
         <button
           type="submit"
