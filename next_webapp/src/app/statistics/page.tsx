ade790d25aa4badeb6180868b5fd3cf674cc12b0
Yhxiao <xiao2364390271@icloud.com>
2024-05-09 16:54:06 +1000

    Dark/light_Switch&StatisticPage_UI_DEV_VictorXiao_#PR3
    Statistics page UI -development] https://trello.com/c/2OEiIsAi/541-statistics-page-ui-development-max-2
    
    Created Barplot for Popularity Range
    Created filter drop list for years
    Created filter drop list for trimesters
    Fixed the issue that search function, now allowing users to search regardless of the case used.
    
    Dark/light_theme_Switch
    https://trello.com/c/KbnJV6Gx/572-dark-light-mode-switch-function
    - Developed theme change feature which can change web page theme automatically based on user's preferred theme.
    - Deployed theme change feature in footer.
    - Deployed theme change feature in statistic page.

--- next_webapp/src/app/statistics/page.tsx
+++ next_webapp/src/app/statistics/page.tsx
@@ -29,15 +29,15 @@ import {
 const Statistics = () => {
     // Dummy Array
     const caseStudies = [
-        { id: 1, tag: 'Safety and Well-being', publishNumber: '4', popularity: '11%', trimester: '1'},
-        { id: 2, tag: 'Environment and Sustainability', publishNumber: '5', popularity: '20%',trimester: '2'},
-        { id: 3, tag: 'Business and activity', publishNumber: '8', popularity: '90%',trimester: '3'},
-        { id: 4, tag: 'Safety and Well-being', publishNumber: '4', popularity: '11%',trimester:'2'},
-        { id: 5, tag: 'Environment and Sustainability', publishNumber: '5', popularity: '20%',trimester:'3'},
-        { id: 6, tag: 'Business and activity', publishNumber: '8', popularity: '90%',trimester:'2'},
-        { id: 7, tag: 'Safety and Well-being', publishNumber: '4', popularity: '11%',trimester:'2'},
-        { id: 8, tag: 'Environment and Sustainability', publishNumber: '5', popularity: '20%',trimester:'1'},
-        { id: 9, tag: 'Business and activity', publishNumber: '8', popularity: '90%',trimester:'2'}
+        { id: 1, tag: 'Safety and Well-being', publishNumber: '4', popularity: '11%', trimester: '1', year: "2023"},
+        { id: 2, tag: 'Environment and Sustainability', publishNumber: '5', popularity: '20%',trimester: '2', year: "2023"},
+        { id: 3, tag: 'Business and activity', publishNumber: '8', popularity: '90%',trimester: '3', year: "2023"},
+        { id: 4, tag: 'Safety and Well-being', publishNumber: '4', popularity: '11%',trimester:'2', year: "2023"},
+        { id: 5, tag: 'Environment and Sustainability', publishNumber: '5', popularity: '20%',trimester:'3', year: "2023"},
+        { id: 6, tag: 'Business and activity', publishNumber: '8', popularity: '90%',trimester:'2', year: "2023"},
+        { id: 7, tag: 'Safety and Well-being', publishNumber: '4', popularity: '11%',trimester:'2', year: "2023"},
+        { id: 8, tag: 'Environment and Sustainability', publishNumber: '5', popularity: '20%',trimester:'1', year: "2023"},
+        { id: 9, tag: 'Business and activity', publishNumber: '8', popularity: '90%',trimester:'2', year: "2023"}
         
     ];
      
@@ -49,15 +49,51 @@ const Statistics = () => {
     const [publishFilter, setPublishFilter] = useState('');
     const [popularityFilter, setPopularityFilter] = useState('');
     const [pagefilter, setPageFilter] = useState('5');
-    const [serach, setSearchTerm] = useState('');
-
-    // Distinct tags for the dropdown
-    const tags = Array.from(new Set(caseStudies.map(study => study.tag)));
+    const [search, setSearchTerm] = useState('');
+    const [yearFilter, setYearFilter] = useState('');
+    const [trimesterFilter, setTrimesterFilter] = useState('');
 
     
 
+    // Distinct tags for the dropdown
+    const tags = useMemo(() => Array.from(new Set(caseStudies.map(study => study.tag))), [caseStudies]);
+    const years = useMemo(() => Array.from(new Set(caseStudies.map(study => study.year))), [caseStudies]);
+    const trimesters = useMemo(() => ['1', '2', '3'], []);
+
+
+
+    const popularityByTag = useMemo(() => {
+        const sumPopularity: {[key: string]: number} = {};
+        const tagCounts: {[key: string]: number} = {};
+        caseStudies.forEach(study => {
+            const popularity = parseFloat(study.popularity.replace('%', ''));
+            if (sumPopularity[study.tag]) {
+                sumPopularity[study.tag] += popularity;
+                tagCounts[study.tag] += 1;
+            } else {
+                sumPopularity[study.tag] = popularity;
+                tagCounts[study.tag] = 1;
+            }
+        });
+
+        return Object.keys(sumPopularity).map(tag => ({
+            tag: tag,
+            avgPopularity: sumPopularity[tag] / tagCounts[tag]
+        }));
+    }, [caseStudies]);
+
+    const [theme, setTheme] = useState('light'); // Default theme
+
     // Effect to handle filtering based on tag, publish number, and popularity
     useEffect(() => {
+        const matchDark = window.matchMedia('(prefers-color-scheme: dark)');
+        const handleChange = (e: { matches: any; }) => {
+            setTheme(e.matches ? 'dark' : 'light');
+        };
+        matchDark.addEventListener('change', handleChange);
+        setTheme(matchDark.matches ? 'dark' : 'light');
+        
+
         let filtered = caseStudies;
         if (tagFilter) {
             filtered = filtered.filter(study => study.tag === tagFilter);
@@ -96,9 +132,18 @@ const Statistics = () => {
                 }
             });
         }
+        if (yearFilter) {
+            filtered = filtered.filter(study => study.year === yearFilter);
+        }
+        if (trimesterFilter) {
+            filtered = filtered.filter(study => study.trimester === trimesterFilter);
+        }
         setFilteredStudies(filtered);
+        return () => {
+            matchDark.removeEventListener('change', handleChange);
+        };
         
-    }, [tagFilter, publishFilter, popularityFilter]);
+    }, [tagFilter, publishFilter, popularityFilter, yearFilter, trimesterFilter]);
 
     const [currentPage, setCurrentPage] = useState(1);
     const recordsPage = parseInt(pagefilter);
@@ -108,8 +153,14 @@ const Statistics = () => {
     const npage = Math.ceil(filteredStudies.length/recordsPage);
    // const numbers = [...Array(npage + 1).keys()].slice(1);
 
+   const themeStyles = {
+    backgroundColor: theme === 'dark' ? '#333' : '#fff',
+    color: theme === 'dark' ? '#fff' : '#333',
+    // Other styles can be added here based on theme
+    };
+
    // Counting the values of trimester to plot on the graph
-   const tri1 = caseStudies.filter(item => item.trimester== '1').length;
+    const tri1 = caseStudies.filter(item => item.trimester== '1').length;
     const tri2 = caseStudies.filter(item => item.trimester== '2').length;
     const tri3 = caseStudies.filter(item => item.trimester== '3').length
 
@@ -128,6 +179,20 @@ const Statistics = () => {
     ],
   };
 
+  const data2 = {
+    labels: popularityByTag.map(item => item.tag),
+    datasets: [
+        {
+            label: 'Average Popularity',
+            backgroundColor: 'rgba(255, 99, 132, 0.6)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+            data: popularityByTag.map(item => item.avgPopularity),
+        },
+    ],
+    };
+
+
   const options = {
     scales: {
       y: {
@@ -139,24 +204,30 @@ const Statistics = () => {
 
     return (
         <div style={{ margin: '0 auto', minHeight: '100vh', display: 'flex', flexDirection: 'column' }} className="font-sans bg-gray-100 text-black">
-            <Header />
-            <h1 className='text-7xl font-bold px-[2rem] pt-[1rem] pb-[4rem]'> Statistics </h1>
-                <div className='flex '>
+            <Header/>
+            <h1 style={themeStyles} className='text-7xl font-bold px-[2rem] pt-[1rem] pb-[4rem]'>Statistics</h1>
+                <div style={themeStyles} className='flex '>
                     <div className='bg-white shadow-2xl ml-[10rem] h-[22rem] w-[40rem] mb-[5rem] pb-[10rem]'>
                         <h4 className='m-10 font-bold text-[25px]'>Trimester which published the most test cases </h4>
                         <div className='mx-5'>
                         <Bar data={data} height={"25%"} width={"90%"}options={options}/>
                         </div>
                     </div>
+                    <div className='bg-white shadow-2xl ml-[10rem] h-[22rem] w-[40rem] mb-[5rem] pb-[10rem]'>
+                        <h4 className='m-10 font-bold text-[25px]'>Popularity by Tag</h4>
+                        <div className='mx-5'>
+                        <Bar data={data2} height={"25%"} width={"90%"}options={options}/>
+                        </div>
+                    </div>
                 </div>
             <main style={{ flex: '1 0 auto', width: '100%' }}>
-                <div style={{padding: '0 50px' }}>
-                    
-                    <section>
+                <div style={{ ...themeStyles, padding: '0 50px' }}>
+                    <section >
                         <select 
                             value={tagFilter} 
                             onChange={(e) => setTagFilter(e.target.value)} 
                             className="p-2 m-2 border shadow-lg"
+                            style={themeStyles}
                         >
                             <option value="">All Tags</option>
                             {tags.map(tag => (
@@ -169,6 +240,7 @@ const Statistics = () => {
                             value={publishFilter}
                             onChange={(e) => setPublishFilter(e.target.value)}
                             className="p-2 m-2 border shadow-lg"
+                            style={themeStyles}
                         >
                             <option value="">All Publish Numbers</option>
                             <option value="lessThan4">Less than 4</option>
@@ -179,6 +251,7 @@ const Statistics = () => {
                             value={popularityFilter}
                             onChange={(e) => setPopularityFilter(e.target.value)}
                             className="p-2 m-2 border shadow-lg"
+                            style={themeStyles}
                         >
                             <option value="">All Popularity Ranges</option>
                             <option value="0to20">0 - 20%</option>
@@ -187,10 +260,24 @@ const Statistics = () => {
                             <option value="60to80">60 - 80%</option>
                             <option value="80to100">80 - 100%</option>
                         </select>
+
+                        <select value={yearFilter} onChange={(e) => setYearFilter(e.target.value)} className="p-2 m-2 border shadow-lg"  style={themeStyles}>
+                            <option value="">All Years</option>
+                              {years.map(year => (
+                            <option key={year} value={year}>{year}</option>
+                            ))}
+                        </select>
+
+                        <select value={trimesterFilter} onChange={(e) => setTrimesterFilter(e.target.value)} className="p-2 m-2 border shadow-lg"  style={themeStyles}>
+                            <option value="">All Trimesters</option>
+                              {trimesters.map(trimester => (
+                            <option key={trimester} value={trimester}>{'Trimester ' + trimester}</option>
+                            ))}
+                        </select>
                         
-                        <div className='flex'>
-                                <div className='border-solid bg-white shadow-2xl border-2 border-black-600 py-8 px-10 my-10'>
-                                <h2 className=' text-2xl font-bold text-gray-400 '>Total Results</h2>
+                        <div className='flex' >
+                                <div style={themeStyles} className='border-solid bg-white shadow-2xl border-2 border-black-600 py-8 px-10 my-10'>
+                                <h2  className=' text-2xl font-bold text-gray-400 '>Total Results</h2>
                                 <p className='text-[1.8rem] font-bold text-center pt-[15px] px-2rem font-bold text-black-400 '>{filteredStudies.length}</p>
                                 </div>
                                 <div>
@@ -203,31 +290,37 @@ const Statistics = () => {
                               type="search"
                                  placeholder="Enter Tag name"
                                  className="w-full px-4 py-2 mr-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-green-500"
-                                onChange={(e)=> setSearchTerm(e.target.value)}
+                                 onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
                             />
                         </form>
-                            <table className="min-w-full bg-white">
-                                <thead>
+                            <table  className="min-w-full bg-white">
+                                <thead >
                                     <tr>
-                                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider rounded-tl-lg">
+                                        <th style={themeStyles} className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider rounded-tl-lg">
                                             ID
                                         </th>
-                                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider rounded-tl-lg">
+                                        <th style={themeStyles} className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider rounded-tl-lg">
                                             Tag
                                         </th>
-                                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                        <th  style={themeStyles} className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                                             Number of Case Studies Published
                                         </th>
-                                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider rounded-tr-lg">
+                                        <th  style={themeStyles} className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider rounded-tr-lg">
                                             Popularity
+                                        </th>
+                                        <th style={themeStyles} className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                            Year
+                                        </th>
+                                        <th style={themeStyles} className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider rounded-tr-lg">
+                                            Trimester
                                         </th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     {records.filter((item)=>{
-                                        return serach.toLowerCase() === ''? item : item.tag.toLowerCase().includes(serach);
+                                        return search.toLowerCase() === ''? item : item.tag.toLowerCase().includes(search);
                                     }).map((study, index) => (
-                                        <tr key={study.id} className={index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}>
+                                        <tr  style={themeStyles} key={study.id} className={index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}>
                                             <td className="px-5 py-5 border-b border-gray-200 text-sm">
                                                 {study.id}
                                             </td>
@@ -240,12 +333,18 @@ const Statistics = () => {
                                             <td className="px-5 py-5 border-b border-gray-200 text-sm">
                                                 {study.popularity}
                                             </td>
+                                            <td className="px-5 py-5 border-b border-gray-200 text-sm">
+                                                {study.year}
+                                            </td>
+                                            <td className="px-5 py-5 border-b border-gray-200 text-sm">
+                                                {study.trimester}
+                                            </td>
                                         </tr>
                                     ))}
                                 </tbody>
                             </table>
                         </div>
-                        <nav className='bg-gray-200 py-3'>
+                        <nav  style={themeStyles} className='bg-gray-200 py-3'>
                             <ul className = 'pagei'>
                             <li className='page-item'>
                                 <button className="  float-right  text-black font-bold py-1 px-2 "><a href='#' className='page-link' onClick={nextPage}>{'>'}</a></button>
@@ -257,8 +356,8 @@ const Statistics = () => {
                                     </button>
                                 </li>
                                
-                                <li className='float-right py-2'>
-                                    <select value={pagefilter} 
+                                <li  className='float-right py-2'>
+                                    <select  style={themeStyles} value={pagefilter} 
                                      onChange={(e) => setPageFilter(e.target.value)} 
                                     >
                                         <option value="5">5</option>
@@ -268,7 +367,7 @@ const Statistics = () => {
                                     </select>
                                 </li>
                                 <li  className='float-right'><p className='px-3 py-1'>Rows per page</p></li>
-                                <li>
+                                <li >
                                 <p>{firstindex+1} - {lastindex} of {filteredStudies.length}</p>
                                 </li>
                                
@@ -277,7 +376,7 @@ const Statistics = () => {
                     </section>
                 </div>
             </main>
-            <Footer />
+            <Footer/>
         </div>
     );
 
--- next_webapp/src/components/Footer.tsx
+++ next_webapp/src/components/Footer.tsx
@@ -1,27 +1,53 @@
 // Footer.js
-import React from 'react';
+"use client"
+import React, { useState, useEffect } from 'react';
 
 const Footer = () => {
+    const [theme, setTheme] = useState('light'); // Default to light theme
+
+    useEffect(() => {
+        const matchDark = window.matchMedia('(prefers-color-scheme: dark)');
+        const handleChange = (e: { matches: any; }) => {
+            setTheme(e.matches ? 'dark' : 'light');
+        };
+        // Listen for changes in the theme preference
+        matchDark.addEventListener('change', handleChange);
+
+        // Set initial theme based on user preference
+        setTheme(matchDark.matches ? 'dark' : 'light');
+
+        // Cleanup function to remove the event listener
+        return () => {
+            matchDark.removeEventListener('change', handleChange);
+        };
+    }, []);
+
+    const themeStyles = {
+        background: theme === 'dark' ? '#333' : '#fff',
+        color: theme === 'dark' ? '#fff' : '#333',
+    };
+
     return (
-        <footer class="bg-white text-black justify-center">
-            <div class="w-full px-32 py-7 flex items-center">
-                <div class="flex-shrink-0 mr-4">
-                    <img src='/img/header-logo.png' alt='logo' class='w-20' />
+        <footer style={themeStyles} className="justify-center">
+            <div style={{ width: '100%', padding: '32px', display: 'flex', alignItems: 'center' }}>
+                <div style={{ marginRight: '16px' }}>
+                    <img src='/img/header-logo.png' alt='logo' style={{ width: '80px' }} />
                 </div>
-                <div class="flex-grow text-center">
-                    <ul class="flex justify-center gap-20">
-                        <a href='/privacypolicy'><li>Privacy Policy</li></a>
-                        <a href='/licensing'><li>Licensing</li></a>
-                        <a href='/contact'><li>Contact Us</li></a>
+                <div style={{ flexGrow: 1, textAlign: 'center' }}>
+                    <ul style={{ display: 'flex', justifyContent: 'center', gap: '80px', listStyle: 'none', padding: 0 }}>
+                        <a href='/privacypolicy' style={{ color: themeStyles.color }}><li>Privacy Policy</li></a>
+                        <a href='/licensing' style={{ color: themeStyles.color }}><li>Licensing</li></a>
+                        <a href='/contact' style={{ color: themeStyles.color }}><li>Contact Us</li></a>
                     </ul>
                 </div>
-                <div class="flex-shrink-0 ml-4 flex gap-2">
-                    <a href='https://data.melbourne.vic.gov.au/pages/home/'>Melbourne Open Data</a>
-                    <img src='https://www.svgrepo.com/show/510970/external-link.svg' alt='link icon' class='w-5' />
+                <div style={{ marginLeft: '16px', display: 'flex', gap: '8px' }}>
+                    <a href='https://data.melbourne.vic.gov.au/pages/home/' style={{ color: themeStyles.color }}>
+                        Melbourne Open Data
+                    </a>
+                    <img src='https://www.svgrepo.com/show/510970/external-link.svg' alt='link icon' style={{ width: '20px' }} />
                 </div>
             </div>
         </footer>
-
     );
 };
 
