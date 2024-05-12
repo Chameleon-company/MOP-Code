cb05e92a2a23c1c7d37b7a5ce02576d02ff799ca
Kasey <kaseywu130@gmail.com>
2024-05-12 20:46:30 +1000

    WD|FIX|failed deployment fix

--- next_webapp/src/app/layout.tsx
+++ next_webapp/src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,12 +10,8 @@ export const metadata: Metadata = {
 
 export default async function LocaleLayout({
   children,
-  params: { locale },
 }: {
   children: React.ReactNode;
-  params: { locale: string };
 }) {
-  const messages = await getMessages();
-
   return <>{children}</>;
 }
