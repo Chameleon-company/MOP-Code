ea61f2f9461de2b5dc7ec47019e3ca0200293dd0
abhishek-chiluka99 <chilukaabhishek9@gmail.com>
2024-05-14 08:21:04 +1000

    Resolve merge conflicts with the new origin code

--- next_webapp/src/components/Header.tsx
+++ next_webapp/src/components/Header.tsx
@@ -3,9 +3,13 @@ import React, { useState } from "react";
 import LanguageDropdown from "../components/LanguageDropdown";
 import { Link } from "@/i18n-navigation";
 import { useTranslations } from "next-intl";
+import { CiMenuBurger  } from "react-icons/ci";
+import { HiMiniXMark } from "react-icons/hi2";
 
 const Header = () => {
   const t = useTranslations("common");
+  let[isOpen, setisOpen] = useState(false);
+ 
 
   return (
     <header>
@@ -16,12 +20,12 @@ const Header = () => {
               <a href="/">
                 <img src="/img/new-logo-white.png" className="h-20" alt="MOP logo" />
               </a>
-              <div onClick={()=>handleChange()} className="w-10 h-7 absolute right-8 top-6 cursor-pointer md:hidden">
+              <div onClick={()=>setisOpen(!isOpen)}className="w-10 h-7 absolute right-8 top-6 cursor-pointer md:hidden">
                 {
                   isOpen ? <HiMiniXMark className="size-8"/>: <CiMenuBurger className="size-6"/>  
                 }
               </div>
-              <ul className={`md:flex justify-between md:pl-9 bg-green-400  md:w-auto w-full absolute md:static md:z-auto  z-[1] ${isOpen ? 'top-12' : 'top-[-430px]'}` }>
+              <ul className={`md:flex justify-between md:pl-9 bg-green-400  md:w-auto w-full absolute md:static md:z-auto  z-[1] ${isOpen ? 'top-18' : 'top-[-430px]'}` }>
                 <li className="">
                   <a
                     href="/"
@@ -31,7 +35,7 @@ const Header = () => {
                     {t("Home")}
                   </a>
                 </li>
-                <li className="inline-block">
+                <li className=" md:inline-block">
                   <Link
                     href="/about"
                     className="rounded-3xl hover:bg-[#287405] block font-serif py-4 px-5 text-white  text-lg"
@@ -39,7 +43,7 @@ const Header = () => {
                     {t("About Us")}
                   </Link>
                 </li>
-                <li className="inline-block">
+                <li className=" md:inline-block">
                   <Link
                     href="/UseCases"
                     className="rounded-3xl hover:bg-[#287405] block font-serif py-4 px-5 text-white  text-lg"
@@ -47,7 +51,7 @@ const Header = () => {
                     {t("Use Cases")}
                   </Link>
                 </li>
-                <li className="inline-block">
+                <li className=" md:inline-block">
                   <Link
                     href="/statistics"
                     className="rounded-3xl hover:bg-[#287405] block font-serif py-4 px-5 text-white  text-lg"
@@ -55,7 +59,7 @@ const Header = () => {
                     {t("Statistics")}
                   </Link>
                 </li>
-                <li className="inline-block">
+                <li className=" md:inline-block">
                   <Link
                     href="/upload"
                     className="rounded-3xl hover:bg-[#287405] block font-serif py-4 px-5 text-white  text-lg"
@@ -70,19 +74,24 @@ const Header = () => {
               <div className=" " x-data="{ open: false }">
                 <LanguageDropdown />
               </div>
+              </li>
+              <li className=" mt-5 sm:mt-3">
               <Link
                 href="/signup"
                 className="border-[1px] border-solid border-white mr-3 font-serif py-3 px-6 mx-3 text-white rounded-full  text-lg"
               >
                 {t("Sign Up")}
               </Link>
+              </li>
+              <li className="mt-8 sm:mt-3">
               <Link
                 href="/login"
                 className="border-[1px] border-solid border-white bg-white text-[#09bd09] font-serif py-3 px-6 mx-3 rounded-full  text-lg"
               >
                 {t("Log In")}
               </Link>
-            </div>
+              </li>
+            </ul>
           </div>
         </nav>
       </div>
