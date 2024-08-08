5ff39cfe226dfe108c356a4b52a66fa2089dd2b6
chaya221071557 <s221071557@deakin.edu.au>
2024-08-09 01:00:22 +1000

    SignUp Page Multi-language functionality

--- next_webapp/src/app/[locale]/signup/page.tsx
+++ next_webapp/src/app/[locale]/signup/page.tsx
@@ -1,11 +1,13 @@
 // SignUpPage.js
-// SignUpPage.js
 "use client";
 
 import { Link } from "@/i18n-navigation";
 import React, { useState } from "react";
+import LanguageDropdown  from "../../../components/LanguageDropdown";
+import { useTranslations } from "next-intl";
 
 const SignUpPage = () => {
+  const t = useTranslations("signup");
   const [password, setPassword] = useState("");
   const [passwordStrength, setPasswordStrength] = useState("");
 
@@ -17,21 +19,21 @@ const SignUpPage = () => {
 
   const checkPasswordStrength = (password) => {
     if (password.length < 6) {
-      return "Weak";
+      return t("Weak");
     } else if (password.length < 10) {
-      return "Moderate";
+      return t("Moderate");
     } else {
-      return "Strong";
+      return t("Strong");
     }
   };
 
   const getPasswordStrengthColor = () => {
     switch (passwordStrength) {
-      case "Weak":
+      case t("Weak"):
         return "red";
-      case "Moderate":
+      case t("Moderate"):
         return "orange";
-      case "Strong":
+      case t("Strong"):
         return "green";
       default:
         return "transparent";
@@ -48,13 +50,16 @@ const SignUpPage = () => {
     <div className="flex flex-col items-center justify-center min-h-screen">
       {/* Already a member section */}
       <div className="absolute top-20 right-20 ml-4 m-4 flex items-center text-[#666666]">
-        <p className="text-sm mr-2">Already a member?</p>
+        <p className="text-sm mr-2">{t("Already a member?")}</p>
         <a
           href="/en/login"
           className="border border-gray-600 text-gray-600 px-4 py-2 ml-4 hover:border-[#999999]"
         >
-          Log In
+          {t("Log In")}
         </a> {/* Changed Link to a */}
+        <div className="ml-4">
+        <LanguageDropdown />
+        </div>
       </div>
 
       {/* Logo */}
@@ -66,21 +71,21 @@ const SignUpPage = () => {
       {/* Sign-up form */}
       <div className="p-8 rounded-lg mt-12">
         <div className="text-center mb-8">
-          <h2 className="text-2xl font-bold mb-12">Account Sign Up</h2>
+          <h2 className="text-2xl font-bold mb-12">{t("Account Sign Up")}</h2>
         </div>
         <div className="mb-4 flex">
-          <label htmlFor="firstNameInput" className="sr-only">First Name</label>
+          <label htmlFor="firstNameInput" className="sr-only">"First Name"</label>
           <input
             type="text"
             id="firstNameInput"
-            placeholder="First name"
+            placeholder={t("First name")}
             className="w-1/2 p-2 rounded-md border-solid border-2 border-[#ccc] mr-2 bg-[#e9ebeb]"
           />
           <label htmlFor="lastNameInput" className="sr-only">Last Name</label>
           <input
             type="text"
             id="lastNameInput"
-            placeholder="Last name"
+            placeholder={t("Last name")}
             className="w-1/2 p-2 rounded-md border-solid border-2 border-[#ccc] mr-2 bg-[#e9ebeb]"
           />
         </div>
@@ -89,7 +94,7 @@ const SignUpPage = () => {
           <input
             type="email"
             id="emailInput"
-            placeholder="Email"
+            placeholder={t("Email")}
             className="w-full p-2 rounded-md border-solid border-2 border-[#ccc] mr-2 bg-[#e9ebeb]"
           />
         </div>
@@ -98,7 +103,7 @@ const SignUpPage = () => {
           <input
             type="password"
             id="passwordInput"
-            placeholder="Password"
+            placeholder={t("Password")}
             className="w-full p-2 rounded-md border-solid border-2 border-[#ccc] mr-2 bg-[#e9ebeb]"
             onChange={handlePasswordChange}
           />
@@ -109,11 +114,11 @@ const SignUpPage = () => {
             <div className="h-full rounded" style={passwordStrengthStyle}></div>
           </div>
           <p className="text-right text-sm text-gray-600 mt-2">
-            Password Strength: {passwordStrength}
+            {t("Password Strength")}: {passwordStrength}
           </p>
         </div>
         <button className="w-full bg-green-500 text-white py-2 px-2 rounded-md cursor-pointer">
-          Next
+          {t("Next")}
         </button>
       </div>
     </div>
