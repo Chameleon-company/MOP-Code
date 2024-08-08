49d1059101852d8a7bad412d407bbcbccbe4d9a2
Tyrepo1 <dimonkey1912@gmail.com>
2024-08-08 13:41:11 +1000

    Eslint + eslint bug fixes

--- next_webapp/src/app/[locale]/UseCases/page.tsx
+++ next_webapp/src/app/[locale]/UseCases/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Header from "../../../components/Header";
 import Footer from "../../../components/Footer";
 import SearchBar from "./searchbar";
