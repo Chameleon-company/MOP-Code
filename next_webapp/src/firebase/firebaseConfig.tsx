dd584c32dc18ce7958ed46e137a7c928c180ee1e
Ahmad Subktageen <102925222+ahmadjanjua1@users.noreply.github.com>
2024-08-09 16:17:05 +1000

    Fix the Contact page responsiveness issue and Message Functionality

--- next_webapp/src/firebase/firebaseConfig.tsx
+++ next_webapp/src/firebase/firebaseConfig.tsx
@@ -1,17 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDHqnuaPwnL7kY_Wr3CZaIJIit8pUMUQxg",
-  authDomain: "mop-db-1e22a.firebaseapp.com",
-  projectId: "mop-db-1e22a",
-  storageBucket: "mop-db-1e22a.appspot.com",
-  messagingSenderId: "436444330801",
-  appId: "1:436444330801:web:2ce33edf0ac26fa60fce10"
+  apiKey: "AIzaSyBq3czH7l3j10iMBTjdtVuI0zR2jIupA0o",
+  authDomain: "openplayground-4a91d.firebaseapp.com",
+  projectId: "openplayground-4a91d",
+  storageBucket: "openplayground-4a91d.appspot.com",
+  messagingSenderId: "387323207322",
+  appId: "1:387323207322:web:301ef71134844f33060923",
+  measurementId: "G-9C95XRXQ4P"
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export { db, collection, addDoc };
+
