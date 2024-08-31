6ddf7ed1be7da4a52ec8210814b49cd6e4865012
Adrian Weerasinghe <adrian97.thilina@gmail.com>
2024-08-31 17:37:32 +1000

    Added NLP to the chatbot

--- next_webapp/src/app/[locale]/signup/page.tsx
+++ next_webapp/src/app/[locale]/signup/page.tsx
@@ -2,8 +2,8 @@
 
 import React, { useState } from "react";
 
-import Header from "@/components/header";
-import Footer from "@/components/footer";
+import Header from "../../../components/Header";
+import Footer from "../../../components/Footer";
 import LanguageDropdown  from "../../../components/LanguageDropdown";
 import { useTranslations } from "next-intl";
 
