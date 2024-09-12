9374e4d0bcb0a53e23e1267f801bf0b42785ad04
chaya221071557 <s221071557@deakin.edu.au>
2024-09-12 16:13:48 +1000

    login

--- next_webapp/src/app/[locale]/login/page.tsx
+++ next_webapp/src/app/[locale]/login/page.tsx
@@ -2,8 +2,11 @@
 
 import React, { useState } from 'react';
 import '../../../../public/styles/login.css';
+import Header from "../../../components/Header";
+import { useTranslations } from "next-intl";
 
 function LoginForm() {
+    const t = useTranslations("login");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [passwordVisible, setPasswordVisible] = useState(false);
@@ -30,55 +33,56 @@ function LoginForm() {
     };
 
     return (
-        <div className="login-container">
-            <div className="top-bar">
-                <img src="/img/image.png" alt="Chameleon Logo" className="logo" />
-                <div className="signup-container">
-                    <span className="no-account">No Account yet?</span>
-                    <a href="./signup" className="sign-up-button">Sign Up</a>
+        <>
+            <div className="w-full fixed top-0 bg-white z-50">
+                <Header /> 
+            </div>
+            <div className="login-container">
+                <div className="top-bar">
+                    <img src="/img/image.png" alt="Chameleon Logo" className="logo" />
+                </div>
+                <div className="login-content mt-20">
+                    <h1 className="login-title">{t("Account Log In")}</h1>
+                    <p className="login-subtitle">{t("Please login to continue to your account")}</p>
+                    <form onSubmit={handleSubmit} action="/submit-your-login-form" method="POST">
+                        <div className="input-group">
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                required
+                                placeholder={t("Email")}
+                                value={email}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="input-group">
+                            <input
+                                type={passwordVisible ? "text" : "password"}
+                                id="password"
+                                name="password"
+                                required
+                                placeholder={t("Password")}
+                                value={password}
+                                onChange={handleChange}
+                            />
+                            <span className="toggle-password" onClick={togglePasswordVisibility}>
+                                {passwordVisible ? "🔒" : "🔓"}
+                            </span>
+                        </div>
+                        <div className="options-container">
+                            <label className="checkbox-label remember-me">
+                                <input type="checkbox" id="remember-me" name="remember-me" />
+                                {t("Remember Me")}
+                            </label>
+                            <a href="#" className="forgot-password">{t("Forgot Password?")}</a>
+                        </div>
+                        <button type="submit" className="login-button">{t("LOGIN")}</button>
+                    </form>
+                    {error && <div className="error text-red-500">{error}</div>}
                 </div>
             </div>
-            <div className="login-content">
-                <h1 className="login-title">Account Log In</h1>
-                <p className="login-subtitle">Please login to continue to your account</p>
-                <form onSubmit={handleSubmit} action="/submit-your-login-form" method="POST">
-                    <div className="input-group">
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            required
-                            placeholder="Email"
-                            value={email}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="input-group">
-                        <input
-                            type={passwordVisible ? "text" : "password"}
-                            id="password"
-                            name="password"
-                            required
-                            placeholder="Password"
-                            value={password}
-                            onChange={handleChange}
-                        />
-                        <span className="toggle-password" onClick={togglePasswordVisibility}>
-                            {passwordVisible ? "🔒" : "🔓"}
-                        </span>
-                    </div>
-                    <div className="options-container">
-                        <label className="checkbox-label remember-me">
-                            <input type="checkbox" id="remember-me" name="remember-me" />
-                            Remember Me
-                        </label>
-                        <a href="#" className="forgot-password">Forgot Password?</a>
-                    </div>
-                    <button type="submit" className="login-button">LOGIN</button>
-                </form>
-                {error && <div className="error text-red-500">{error}</div>}
-            </div>
-        </div>
+        </>
     );
 }
 
