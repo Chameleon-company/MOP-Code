5ca93a9fce0de4f10e27d3ed3d220db79b53e39e
md0311 <dmihir8000@gmail.com>
2024-09-15 04:39:17 +1000

    Responsivesness fix in footer.tsx, dashboard.tsx and dashboardcasestd.tsx

--- next_webapp/src/components/Dashboard.tsx
+++ next_webapp/src/components/Dashboard.tsx
@@ -4,233 +4,6 @@ import mainimage from "../../public/img/mainImage.png";
 import secondimage from "../../public/img/second_image.png";
 import { useTranslations } from "next-intl";
 
-const style = `
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
-.nav-menu{
-    display: flex;
-    flex-direction: row;
-    gap: 29px;
-}
-body{
-    background: white;
-}
-
-.nav-menu a{
-    text-decoration: none;
-    color: white;
-}
-.nav-section{
-    background: #2ECC71;
-    display: flex;
-    justify-content: space-between;
-    padding-top: 15px;
-    padding-bottom: 15px;
-    padding-left: 61px;
-    padding-right: 24px;
-    align-items: center;
-    
-
-}
-.left-section{
-    display: flex;
-    gap: 68px;
-    align-items: center;
-}
-
-.left-section .logo{
-    height: 50px;
-    width: 56px;
-}
-.left-section .logo img{
-    width: 100%;
-    height: 100%;
-}
-.sign-up{
-    border: 1px solid white;
-    text-decoration: none;
-    padding: 12px 24px;
-    background: #2ECC71;
-    color: white;
-    border-radius: 14px;
-}
-.log-in{
-    color: #2ECC71;
-    background: white;
-    padding: 12px 24px;
-    border: 1px solid white;
-    border-radius: 14px;
-}
-.nav-section .right-section{
-    display: flex;
-    gap: 27px;
-}
-
-
-
-
-/* ------- */
-
-.hero-section{
-    margin-top: 55px;
-    height: auto;
-    background: grey;
-    overflow: hidden;
-}
-.hero-section img{
-    width: 100%;
-    height : 805px;
-}
-
-
-
-
-
-
-.our-vision-section{
-    display: flex;
-    background: #2ECC71;
-    justify-content: space-between;
-    border: 1px solid white;
-    margin-top: 21px;
-    .our-vision{
-        margin-left: 61px;
-        margin-top: 98px;
-        line-height: 60px;
-        font-family: Montserrat;
-        font-size: 64px;
-        font-weight: 600;
-        letter-spacing: -0.015em;
-        text-align: left;
-        color: white;
-        background: #2ECC71;
-    }
-    .img-div{
-        margin-top: 139px;
-    max-width: 705px;
-    width: 100%;
-    border: 1px solid black;
-    height: 429px;
-    margin-left: 0px;
-    margin-right: 25px;
-    position: relative;
-    }
-    .img-div img{
-        width: 100%;
-        height:100%;
-    }
-    .img-div:after{
-        content: '';
-        width: 342px;
-        height:55px;
-        position: absolute;
-        top: -56px;
-        right: 0;
-        background-color: #000000;
-    }
-    .img-div:before{
-        content: '';
-        width: 45px;
-        height: 243px;
-        position: absolute;
-        background-color: #000000;
-        left: -46px;
-        bottom: -1px;
-    }
-    
-   
-    .text-div{
-        margin-top: 284px;
-        max-width: 686px;
-        width: 100%;
-        font-family: 'Montserrat';
-        font-size: 17px;
-        text-align: left;
-        color: white;
-    }
-}
-.case-studies-wrapper{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 85px;
-}
-.card-wrapper{
-    border: 1px solid black;
-    margin-top: 20px;
-    width: 429px;
-    height: 560px;
-}
-.card-wrapper h4{
-    font-family: Montserrat;
-font-size: 24px;
-font-weight: 700;
-margin-top: 47px;
-margin-left: 64px;
-color:black;
-}
-.card-wrapper p{
-    font-family: Montserrat;
-    font-size: 18px;
-    font-weight: 400;
-    margin-top: 24px;
-    margin-left: 64px;
-    color:black;
-    max-width: 300px;
-    width: 100%;
-}
-
-.card-wrapper .top-image{
-    height: 281px;
-    padding-left: 64px;
-    padding-right:64px;
-    padding-top:64px;
-}
-.card-wrapper .top-image img{
-    width: 100%;
-    height: 100%;
-}
-
-.recent-case-studies{
-    margin-top: 105px;
-    margin-left: 80px;
-    margin-bottom: 82px;
-}
-.recent-case-studies h2{
-    color : black;  
-    font-family: Montserrat;
-    font-size: 48px;
-    font-weight: 700;
-    margin-bottom: 71px;
-}
-.recent-case-studies p{
-    font-family: Montserrat;
-    font-size: 36px;
-    font-weight: 400;
-    color : black;
-
-}
-
-.sign-up-btn-section{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-top: 16px;
-    margin-bottom: 21px;
-}
-.sign-up-btn-section .sign-up-btn{
-    width: 192px;
-    height: 52px;
-    color: white;
-    font-size: 15px;
-    font-weight: 500;
-    background: #2ECC71;
-    border: 0px;
-    border-radius: 10px;
-}
-.main-wrapper{
-    background-color: white;
-}
-`;
 const Dashboard = () => {
   const navItems = [
     { to: "/about", icon: "/img/about-icon.png", label: "About Us" },
@@ -248,31 +21,38 @@ const Dashboard = () => {
 
   return (
     <>
-      <style dangerouslySetInnerHTML={{ __html: style }} />
-
-      <div className="main-wrapper">
-        <div className="main-container">
-          <section className="hero-section">
-            <Image src={mainimage} alt={"main image1"} />
+      <div className="bg-white">
+        <div className="w-full">
+          {/* Hero Section */}
+          <section className="hero-section w-full h-auto mt-12">
+            <Image src={mainimage} alt={"main image"} className="w-full h-auto" />
           </section>
-          <section className="sign-up-btn-section">
-            <button className="sign-up-btn">
+
+          {/* Sign Up Button Section */}
+          <section className="flex justify-center items-center mt-4 mb-5">
+            <button className="bg-green-500 text-white font-medium py-3 px-6 rounded-lg">
               <Link href="signup">{t("Sign Up")}</Link>
             </button>
           </section>
-          <section className="our-vision-section">
-            <div className="our-vision">{t("Our Vision")}</div>
-            <div className="img-div">
-              <Image src={secondimage} alt={"Second Image"} />
+
+          {/* Our Vision Section */}
+          <section className="bg-green-500 text-white py-8 px-4 md:px-16 lg:flex justify-between items-center">
+            <div className="flex flex-col justify-center">
+              <h1 className="text-3xl md:text-5xl font-bold mb-8">{t("Our Vision")}</h1>
+              <p className="text-lg md:text-xl leading-relaxed">{t("intro")}</p>
             </div>
-            <div className="text-div">{t("intro")}</div>
-          </section>
-          <section className="recent-case-studies">
-            <h2>{t("Recent Case Studies")}</h2>
-            <p>{t("p2")}</p>
+            <div className="relative mt-8 lg:mt-0 lg:w-1/2">
+              <Image src={secondimage} alt={"Second Image"} className="w-full h-auto" />
+              <div className="absolute top-0 right-0 w-16 h-16 bg-black"></div>
+              <div className="absolute bottom-0 left-0 w-8 h-40 bg-black"></div>
+            </div>
           </section>
 
-          <section className="case-studies"></section>
+          {/* Recent Case Studies Section */}
+          <section className="recent-case-studies  mt-20 mb-20 px-4 md:px-16">
+            <h2 className="text-black text-4xl font-bold mb-12">{t("Recent Case Studies")}</h2>
+            <p className="text-3xl  text-black">{t("p2")}</p>
+          </section>
         </div>
       </div>
     </>
--- next_webapp/src/components/DashboardCaseStd.tsx
+++ next_webapp/src/components/DashboardCaseStd.tsx
@@ -1,4 +1,3 @@
-// DashboardCaseStd.js
 import React from "react";
 import Link from "next/link";
 import { useTranslations } from "next-intl";
@@ -26,20 +25,22 @@ const DashboardCaseStd = () => {
       image: "/img/icon3.png",
       title: t("Case Study 3"),
       description: t("Data Science in Education"),
-      link: "UseCases",
+      link: "/UseCases",
     },
   ];
 
   return (
-    <div className="case-studies-wrapper">
+    <div className="case-studies-wrapper grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-4">
       {caseStudies.map((caseStudy) => (
         <Link href={`en/${caseStudy.link}`} key={caseStudy.id}>
-          <div className="card-wrapper">
-            <div className="top-image">
-              <img src={caseStudy.image} alt={caseStudy.title} />
+          <div className="card-wrapper bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-300">
+            <div className="top-image h-48 flex justify-center items-center bg-gray-100">
+              <img src={caseStudy.image} alt={caseStudy.title} className="object-contain h-full" />
             </div>
-            <h4 className="title">{caseStudy.title}</h4>
-            <p className="description">{caseStudy.description}</p>
+            <div className="p-6">
+              <h4 className="title text-xl font-semibold mb-3">{caseStudy.title}</h4>
+              <p className="description text-gray-700">{caseStudy.description}</p>
+            </div>
           </div>
         </Link>
       ))}
