51ddc3aee7ab4774cabc6269f9a16570d6bbc142
koiCoding <mkpha.contact@gmail.com>
2024-09-15 18:30:45 +1000

    Adjusted Font Size & Layout for small screen sizes

--- next_webapp/src/app/[locale]/about/page.tsx
+++ next_webapp/src/app/[locale]/about/page.tsx
@@ -20,51 +20,62 @@ const About = () => {
     <div className={`${dark_value && "dark"}`}>
       <div className="bg-white dark:bg-[#1d1919]">
         <Header />
-        <div className="container-title bg-green-500 dark:bg-green-900">
-          <div className="content-title md:py-8">
-            <img
-              src="/img/mel.jpg"
-              alt="Melbourne Open Playground"
-              className="float-right ml-4 img-title"
-            />
-            <div className="text-box font-sans">
-              <h2 className="font-bold">{t("About Us")}</h2>
-              <br />
-              <p className="w-full md:w-[60%]" id="p2">{t("p2")}</p>
+        <div className="h-full content-center">
+          <div className="container-title bg-green-500 dark:bg-green-900">
+            <div className="content-title md:py-8">
+              <img
+                src="/img/mel.jpg"
+                alt="Melbourne Open Playground"
+                className="float-right ml-4 img-title"
+              />
+              <div className="text-box font-sans">
+                <h2 className="font-bold">{t("About Us")}</h2>
+                <br />
+                <p className="w-full md:w-[60%]" id="p2">
+                  {t("p2")}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="container bg-white dark:bg-[#1d1919]">
-          <div className="content-section md:pt-16">
-            <img
-              src="/img/leadership.png"
-              alt="Leadership Image"
-              className="float-right img-section ml-0.5"
-            />
-            <div className="text-box font-sans">
-              <h2 className="font-bold text-black dark:text-white">
-                {t("Open Data Leadership")}
-              </h2>
-              <br />
-              <p className="text-black dark:text-white w-full md:w-[60%]" id="p3">
-                {t("p3")}
-              </p>
+          <div className="container bg-white dark:bg-[#1d1919]">
+            <div className="content-section md:pt-16">
+              <img
+                src="/img/leadership.png"
+                alt="Leadership Image"
+                className="float-right img-section ml-0.5"
+              />
+              <div className="text-box font-sans">
+                <h2 className="font-bold text-black dark:text-white">
+                  {t("Open Data Leadership")}
+                </h2>
+                <br />
+                <p
+                  className="text-black dark:text-white w-full md:w-[60%]"
+                  id="p3"
+                >
+                  {t("p3")}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="container bg-white  dark:bg-[#1d1919]">
-          <div className="content-section md:pt-6">
-            <img
-              src="/img/goals.png"
-              alt="Our Goals"
-              className="float-left img-section pr-[5%]"
-            />
-            <div className="text-box font-sans">
-              <h2 className="font-bold text-black dark:text-white">{t("Our Goals")}</h2>
-              <br />
-              <p className="text-black dark:text-white" id="p4">{t("p4")}</p>
+          <div className="container bg-white  dark:bg-[#1d1919]">
+            <div className="content-section md:pt-6">
+              <img
+                src="/img/goals.png"
+                alt="Our Goals"
+                className="float-left img-section pr-[5%]"
+              />
+              <div className="text-box font-sans">
+                <h2 className="font-bold text-black dark:text-white">
+                  {t("Our Goals")}
+                </h2>
+                <br />
+                <p className="text-black dark:text-white" id="p4">
+                  {t("p4")}
+                </p>
+              </div>
             </div>
           </div>
         </div>
