4570f0ee94d0c7b07ff3b854a018a31f37584c7e
md0311 <dmihir8000@gmail.com>
2024-09-18 01:16:43 +1000

    Resolve conflits

--- next_webapp/src/components/Dashboard.tsx
+++ next_webapp/src/components/Dashboard.tsx
@@ -8,7 +8,216 @@ import { CaseStudy, CATEGORY, SEARCH_MODE, SearchParams } from "@/app/types";
 import { useEffect, useState } from "react";
 import { ArrowLeft, FileText } from "lucide-react";
 
+const style = `
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+.nav-menu{
+    display: flex;
+    flex-direction: row;
+    gap: 29px;
+}
+body{
+    background: white;
+}
 
+.nav-menu a{
+    text-decoration: none;
+    color: white;
+}
+.nav-section{
+    background: #2ECC71;
+    display: flex;
+    justify-content: space-between;
+    padding-top: 15px;
+    padding-bottom: 15px;
+    padding-left: 61px;
+    padding-right: 24px;
+    align-items: center;
+    
+}
+.left-section{
+    display: flex;
+    gap: 68px;
+    align-items: center;
+}
+.left-section .logo{
+    height: 50px;
+    width: 56px;
+}
+.left-section .logo img{
+    width: 100%;
+    height: 100%;
+}
+.sign-up{
+    border: 1px solid white;
+    text-decoration: none;
+    padding: 12px 24px;
+    background: #2ECC71;
+    color: white;
+    border-radius: 14px;
+}
+.log-in{
+    color: #2ECC71;
+    background: white;
+    padding: 12px 24px;
+    border: 1px solid white;
+    border-radius: 14px;
+}
+.nav-section .right-section{
+    display: flex;
+    gap: 27px;
+}
+/* ------- */
+.hero-section{
+    margin-top: 55px;
+    height: auto;
+    background: grey;
+    overflow: hidden;
+}
+.hero-section img{
+    width: 100%;
+    height : 805px;
+}
+.our-vision-section{
+    display: flex;
+    background: #2ECC71;
+    justify-content: space-between;
+    border: 1px solid white;
+    margin-top: 21px;
+    .our-vision{
+        margin-left: 61px;
+        margin-top: 98px;
+        line-height: 60px;
+        font-family: Montserrat;
+        font-size: 64px;
+        font-weight: 600;
+        letter-spacing: -0.015em;
+        text-align: left;
+        color: white;
+        background: #2ECC71;
+    }
+    .img-div{
+        margin-top: 139px;
+    max-width: 705px;
+    width: 100%;
+    border: 1px solid black;
+    height: 429px;
+    margin-left: 0px;
+    margin-right: 25px;
+    position: relative;
+    }
+    .img-div img{
+        width: 100%;
+        height:100%;
+    }
+    .img-div:after{
+        content: '';
+        width: 342px;
+        height:55px;
+        position: absolute;
+        top: -56px;
+        right: 0;
+        background-color: #000000;
+    }
+    .img-div:before{
+        content: '';
+        width: 45px;
+        height: 243px;
+        position: absolute;
+        background-color: #000000;
+        left: -46px;
+        bottom: -1px;
+    }
+    
+   
+    .text-div{
+        margin-top: 284px;
+        max-width: 686px;
+        width: 100%;
+        font-family: 'Montserrat';
+        font-size: 17px;
+        text-align: left;
+        color: white;
+    }
+}
+.case-studies-wrapper{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 85px;
+}
+.card-wrapper{
+    border: 1px solid black;
+    margin-top: 20px;
+    width: 429px;
+    height: 560px;
+}
+.card-wrapper h4{
+    font-family: Montserrat;
+font-size: 24px;
+font-weight: 700;
+margin-top: 47px;
+margin-left: 64px;
+color:black;
+}
+.card-wrapper p{
+    font-family: Montserrat;
+    font-size: 18px;
+    font-weight: 400;
+    margin-top: 24px;
+    margin-left: 64px;
+    color:black;
+    max-width: 300px;
+    width: 100%;
+}
+.card-wrapper .top-image{
+    height: 281px;
+    padding-left: 64px;
+    padding-right:64px;
+    padding-top:64px;
+}
+.card-wrapper .top-image img{
+    width: 100%;
+    height: 100%;
+}
+.recent-case-studies{
+    margin-top: 105px;
+    margin-left: 80px;
+    margin-bottom: 82px;
+}
+.recent-case-studies h2{
+    color : black;  
+    font-family: Montserrat;
+    font-size: 48px;
+    font-weight: 700;
+    margin-bottom: 71px;
+}
+.recent-case-studies p{
+    font-family: Montserrat;
+    font-size: 36px;
+    font-weight: 400;
+    color : black;
+}
+.sign-up-btn-section{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 16px;
+    margin-bottom: 21px;
+}
+.sign-up-btn-section .sign-up-btn{
+    width: 192px;
+    height: 52px;
+    color: white;
+    font-size: 15px;
+    font-weight: 500;
+    background: #2ECC71;
+    border: 0px;
+    border-radius: 10px;
+}
+.main-wrapper{
+    background-color: white;
+}
+`;
 const Dashboard = () => {
     const [filteredCaseStudies, setFilteredCaseStudies] = useState<CaseStudy[]>([]);
     const [selectedCaseStudy, setSelectedCaseStudy] = useState<CaseStudy | undefined>(undefined)
