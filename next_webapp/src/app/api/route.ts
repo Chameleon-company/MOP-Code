1859fe12949aa42826d77e39a46284444481ba47
Manoj-S223817296 <s223817296@deakin.edu.au>
2024-12-13 14:44:45 +1100

    Updated usecases to get from ready to publish and implemented carousel functionality to usecase in homepage

--- next_webapp/src/app/api/route.ts
+++ next_webapp/src/app/api/route.ts
@@ -13,8 +13,6 @@ export async function GET(request: Request) {
 
   // Constructing the File Path
   const filePath = path.join(
-    process.cwd(),
-    "public",
     fileName
   );
   // Reading the file
