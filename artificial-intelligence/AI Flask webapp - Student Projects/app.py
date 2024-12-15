ccab6cddea1fc39654f2d6b3ef4877447cae8636
Chathurni Ratwatte <85515751+cssratwatte@users.noreply.github.com>
2024-12-15 11:52:21 +1100

    Update app.py

--- artificial-intelligence/AI Flask webapp - Student Projects/app.py
+++ artificial-intelligence/AI Flask webapp - Student Projects/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
-import health_safety
 import subjective_wellbeing
 from map import create_map
 from dash_app import create_dash_app
