34d7f0e96150dc6e71b93eeeda5ee849bf3e359d
Danish Kumar <127257306+DanishKumar1001@users.noreply.github.com>
2025-05-06 23:49:56 +1000

    Revert "WD_FrontEnd_Manoj#Header and Footer "

--- next_webapp/src/components/LanguageDropdown.tsx
+++ next_webapp/src/components/LanguageDropdown.tsx
@@ -35,7 +35,6 @@ const LanguageDropdown: React.FC = () => {
     <div ref={ref} className="relative">
       <button
         onClick={() => setIsOpen(!isOpen)}
-              className="mr-4 bg-white text-green-600 hover:bg-gray-50 border border-green-600 px-4 py-2 rounded-xl text-sm font-medium"
         className="mr-2 bg-white text-green-600 hover:bg-gray-50 border border-green-600 px-4 py-2 rounded-md text-sm font-medium dark:bg-gray-900 dark:hover:bg-green-600 dark:hover:text-gray-900 hover:bg-green-700 hover:text-white"
       >
         {t("Language")}
