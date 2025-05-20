aa307e4206bdeb1389ab54b99029cf6e55ba5386
Danish1001 <Danishkumar1001@gmail.com>
2025-05-20 20:38:16 +1000

    Social Media Component Added to Home

--- next_webapp/src/app/[locale]/page.tsx
+++ next_webapp/src/app/[locale]/page.tsx
@@ -3,12 +3,14 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Dashboard from "../../components/Dashboard";
 import Chatbot from "../chatbot/chatbot";
+import SocialMediaFeed from "@/components/SocialMediaFeed";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Dashboard />
+      <SocialMediaFeed />
       <Chatbot />
       <Footer />
     </div>
