7b7c1c47b34561c775330e47c373037fd0b7ccd0
ISHIKA MANDAL <s223973381@deakin.edu.au>
2025-09-17 17:29:55 +1000

    Implementing urban tree planning use case

--- next_webapp/src/utils/data.ts
+++ next_webapp/src/utils/data.ts
@@ -124,7 +124,7 @@ export const useCases = [
         description:
           "Use AI to optimize energy consumption in industries and cities.",
         image: "/img/2bafb41a2f7d47daa950025568befe0218893c90-2880x1440.avif",
-        link: "",
+        link: "/dummypages/Urban_Tree_Planting.html",
       },
       {
         heading: "Waste Efficiency Argyle Square case study",
