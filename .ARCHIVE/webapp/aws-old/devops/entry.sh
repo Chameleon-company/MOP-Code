6faa30b4e9e7b4d2bcd2fa9e202e011be2ed45ef
caseyjmm <caseyjmm10@gmail.com>
2025-09-19 20:26:53 +0930

    Folder name updated to Pedestrian Detection and Crowd Density Estimation

--- .ARCHIVE/webapp/aws-old/devops/entry.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
-    exec aws-lambda-rie python -m awslambdaric $1
-else
-    exec python -m awslambdaric $1
-fi
--- datascience/usecases/READY TO PUBLISH/Urban_Planning_and_Development/2025/T1/README.md
+++
@@ -1,4 +0,0 @@
-# 2025 T1 Use Cases
-
-This folder contains Urban Planning & Development use cases for Trimester 1, 2025.
-
