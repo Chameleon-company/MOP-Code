86f4e49f508c155d722039134b81ee8233f4f0c9
PRATHAM KUNDRA <prathamkundra32@gmail.com>
2025-10-07 15:25:18 +1100

    Add Mistral Chatbot use case

---
+++ artificial-intelligence/T2_2025/Mental Health and Smart City Chatbot/Recycling chatbot/Mistral_ChatBot/src/components/ChatHeader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ChatHeader() {
+  return (
+    <div className="chat-header">
+      <h4>Recycling Chatbot</h4>
+    </div>
+  );
+}
+
+export default ChatHeader;
