acc24568de2fd966e7d1c00e40444bab02629a43
JOSH <s222257678@deakin.edu.au>
2026-04-02 21:23:15 +1100

    Implemented API with the user interface and generated test results to display

--- Playground/Josh_T12026/User_Interface/ui.py
+++ Playground/Josh_T12026/User_Interface/ui.py
@@ -9,6 +9,18 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
+#send images to FastAPI
+def backend_detect(files):
+    url = "http://localhost:8000/detect"
+    response = requests.post(url, files = files)
+    return response.json()
+
+#send detection results to FastAPI
+def backend_report(detection_data):
+    url = "http://localhost:8000/report"
+    response = requests.post(url, json = detection_data)
+    return response.json()
+
 #SIDEBAR
 st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to", ["Homepage", "Detection", "Reports"])
@@ -32,6 +44,9 @@ if page == "Homepage":
 elif page == "Detection": 
     st.title("Streetlight Analysis")
     
+    #hold the result for backend use 
+    stored_result = None
+    
     #COLUMNS
     col1, col2 = st.columns([1, 2])
     
@@ -50,25 +65,43 @@ elif page == "Detection":
             #button for analysis 
             if st.button("Analyse image(s)", type = "primary"):
                 with st.spinner("Analysing images..."):
-                    import time
-                    time.sleep(2)  # simulate processing delay
-
+                    #prep files 
+                    file_data = [
+                        ("files", (file.name, file.getvalue(), file.type))
+                        for f in uploaded_files
+                    ]
+                    
+                    #send to backend
+                    stored_result = backend_detect(file_data)
+                    
+                    st.session_state["analysis_result"] = stored_result 
+                    
                 st.success("Analysis complete!")
+        else:
+            st.info("No uploaded images.")
 
     #DIVIDE PAGE 
     st.markdown("---")
 
     st.header("Analysis Results")
-    st.write("This section will display:")
-    st.markdown("""
-    - Number of detected streetlights  
-    - Number of faulty lights  
-    """)
-
-    st.success("Results placeholder")
+    if "analysis_result" in st.session_state:
+        st.json(st.session_state["analysis_result"])
+    else:
+        st.info("No analysis results")
 
 #REPORTSPAGE
 elif page == "Reports": 
     st.title("Maintenance Report")
     
-    st.info("Report placeholder")
+    if st.button("Generate report", type = "primary"): 
+        if "analysis_result" not in st.session_state: 
+            st.warning("No results available. Please run detection analysis first")
+        else: 
+            with st.spinner("Generating report..."):
+                detection_data = st.session_state["analysis_result"]
+                
+                #send to backend 
+                report = backend_report(detection_data)
+                
+            st.success("Report generated!")
+            st.json(report)
