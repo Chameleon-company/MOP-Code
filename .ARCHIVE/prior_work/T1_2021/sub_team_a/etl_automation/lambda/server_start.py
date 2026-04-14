b1622f5bfc19fe4929caf0c4ebd11dd1deb76e91
Rupanshi <rupanshimanchanda2605@gmail.com>
2026-04-14 23:08:58 +1000

    adding Project-3b group folder and Rupanshi personal folder

--- .ARCHIVE/prior_work/T1_2021/sub_team_a/etl_automation/lambda/server_start.py
+++
@@ -1,8 +0,0 @@
-import boto3
-region = 'us-east-1'
-instances = ['i-025e6cecbf11c7d4d']
-ec2 = boto3.client('ec2', region_name=region)
-
-def lambda_handler(event, context):
-    # ec2.start_instances(InstanceIds=instances)
-    print('started instance: ' + str(instances))
--- datascience/usecases/READY TO PUBLISH/Community_and_Social_Impact/2025/T1/UC00126_Accessibility_Analysis/readme.txt
+++
@@ -1,1 +0,0 @@
-Final Use Case File
