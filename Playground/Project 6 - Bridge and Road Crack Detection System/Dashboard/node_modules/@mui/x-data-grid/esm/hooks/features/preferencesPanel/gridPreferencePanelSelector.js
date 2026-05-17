a4e580f9a05e4c1863a82eb3947bf57984d0b4f7
kamleshthrivin <167185999+kamleshthrivin@users.noreply.github.com>
2026-05-18 04:24:55 +1000

    Update Playground files

--- Playground/Project 6 - Bridge and Road Crack Detection System/Dashboard/node_modules/@mui/x-data-grid/esm/hooks/features/preferencesPanel/gridPreferencePanelSelector.js
+++
@@ -1,8 +0,0 @@
-import { createSelector, createRootSelector } from "../../../utils/createSelector.js";
-export const gridPreferencePanelStateSelector = createRootSelector(state => state.preferencePanel);
-export const gridPreferencePanelSelectorWithLabel = createSelector(gridPreferencePanelStateSelector, (panel, labelId) => {
-  if (panel.open && panel.labelId === labelId) {
-    return true;
-  }
-  return false;
-});
--- Playground/Project 6 - Bridge and Road Crack Detection System/Dashboard/node_modules/@mui/x-data-grid/esm/hooks/features/preferencesPanel/gridPreferencePanelsValue.d.ts
+++
@@ -1,6 +0,0 @@
-declare enum GridPreferencePanelsValue {
-  filters = "filters",
-  columns = "columns",
-  aiAssistant = "aiAssistant",
-}
-export { GridPreferencePanelsValue };
