3065cb811c0ab70314ecc69dce3507438cff3431
lucifer01 <smt.adh@gmail.com>
2026-07-20 23:17:18 +1000

    remove old playground student folder

--- Playground/Project 6 - Bridge and Road Crack Detection System/Dashboard/node_modules/@mui/material/esm/TablePaginationActions/tablePaginationActionsClasses.js
+++
@@ -1,7 +0,0 @@
-import generateUtilityClasses from '@mui/utils/generateUtilityClasses';
-import generateUtilityClass from '@mui/utils/generateUtilityClass';
-export function getTablePaginationActionsUtilityClass(slot) {
-  return generateUtilityClass('MuiTablePaginationActions', slot);
-}
-const tablePaginationActionsClasses = generateUtilityClasses('MuiTablePaginationActions', ['root']);
-export default tablePaginationActionsClasses;
