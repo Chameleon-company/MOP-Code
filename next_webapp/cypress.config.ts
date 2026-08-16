43c7c62e8cc7664f878ff4bd1c920f643cd17acb
betty cheng <yycbetty14@gmail.com>
2026-08-16 23:32:14 +1000

    update cypress.config.ts with correct baseUrl and specPattern

--- next_webapp/cypress.config.ts
+++ next_webapp/cypress.config.ts
@@ -3,15 +3,17 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     // Set the baseUrl to the running Next.js application
-    baseUrl: 'http://localhost:3000',  // Ensure your app runs here during testing
-    
+    // Add locale /en for default testing
+    baseUrl: 'http://localhost:3000/en',  // Ensure your app runs here during testing
+
     // Optional: specify the pattern to look for test files
-    specPattern: 'cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
-    
+    // include e2e and integration test files
+    specPattern: 'cypress/{e2e,integration}/**/*.cy.{js,jsx,ts,tsx}',
+
     // Optional: set the viewport size
     viewportWidth: 1280,
     viewportHeight: 720,
-    
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
