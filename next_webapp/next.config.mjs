e56f7e6b3aab96c7c235b3bea6ceb6834ee1b4a4
HarshanaDeakinUni <s224714499@deakin.edu.au>
2026-08-24 09:13:51 +1000

    docker file update

--- next_webapp/next.config.mjs
+++ next_webapp/next.config.mjs
@@ -4,6 +4,7 @@ const withNextIntl = createNextIntlPlugin("./src/i18n.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   experimental: {
     forceSwcTransforms: true,
   },
