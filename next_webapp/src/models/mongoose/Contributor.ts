2adb2d577babd05a6d65ede22d6623a0f43b5165
suba-t24 <s225094537@deakin.edu.au>
2026-09-08 13:30:27 +1000

    fix: validate contributor position against team

--- next_webapp/src/models/mongoose/Contributor.ts
+++ next_webapp/src/models/mongoose/Contributor.ts
@@ -100,6 +100,13 @@ contributorSchema.pre("validate", function (next) {
     if (!this.level) {
       this.invalidate("level", "level is required for student contributors");
     }
+    
+    if (this.team && this.position) {
+      const validRoles = TEAM_ROLES[this.team as TeamName];
+      if (validRoles && !(validRoles as readonly string[]).includes(this.position)) {
+        this.invalidate("position", `Position '${this.position}' is not valid for team '${this.team}'`);
+      }
+    }
   }
   next();
 });
