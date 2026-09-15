601e1247395074db8ae5e8a8ac9da3e53fd350ee
j0j0-ops <josephgitau230@gmail.com>
2026-09-15 22:20:11 +1000

    fix Sentry config file locations

--- next_webapp/src/instrumentation.ts
+++ next_webapp/src/instrumentation.ts
@@ -2,7 +2,7 @@ import * as Sentry from "@sentry/nextjs";
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    await import("./app/sentry.server.config");
+    await import("./sentry.server.config");
   }
 
   if (process.env.NEXT_RUNTIME === "edge") {
